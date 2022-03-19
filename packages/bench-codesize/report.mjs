f67f95281f2e59607c7a9b7fdc00d8b52b0f1e28
Timo Stamm <ts@timostamm.de>
2022-03-19 02:44:15 +0100

    Make code size benchmark prettier

--- packages/bench-codesize/report.mjs
+++ packages/bench-codesize/report.mjs
@@ -11,13 +11,13 @@ This is a simple code size comparison between Connect-Web and gRPC-web.
 
 We are generating code for the module [buf.build/bufbuild/buf](https://buf.build/bufbuild/buf)
 once with protoc's [built-in JavaScript generator](https://github.com/protocolbuffers/protobuf/blob/7ecf43f0cedc4320c1cb31ba787161011b62e741/src/google/protobuf/compiler/js/js_generator.cc), 
-once with \`protoc-gen-es\`. Then we bundle a snipped of code with [esbuild](https://esbuild.github.io/),
+once with \`protoc-gen-es\`. Then we bundle a [snippet of code](./src) with [esbuild](https://esbuild.github.io/),
 minify the bundle, and compress it like a web server would usually do.
 
-| code generator                         | bundle size        | minified               | gzip               |
-|----------------------------------------|-------------------:|-----------------------:|-------------------:|
-| [protobuf-es](src/entry-protobuf-es.ts) | ${protobufEs.size} | ${protobufEs.minified} | ${protobufEs.brotli} |
-| [google-protobuf](src/entry-google-protobuf.js)       | ${googleProtobuf.size}    | ${googleProtobuf.minified}    | ${googleProtobuf.brotli}    |
+| code generator    | bundle size             | minified               | gzip               |
+|-------------------|------------------------:|-----------------------:|-------------------:|
+| protobuf-es       | ${protobufEs.size}      | ${protobufEs.minified} | ${protobufEs.brotli} |
+| google-protobuf   | ${googleProtobuf.size}  | ${googleProtobuf.minified}    | ${googleProtobuf.brotli}    |
 `);
 
 
