05460013b3a404ba27adc46aba668d7bd9c28dc8
Timo Stamm <ts@timostamm.de>
2022-03-29 15:25:53 +0200

    Cover generated JavaScript
    Extends the tests to cover not only TypeScript generated code, but JavaScript as well.

--- packages/protobuf-test/src/constructor.test.ts
+++ packages/protobuf-test/src/constructor.test.ts
@@ -13,104 +13,115 @@
 // limitations under the License.
 
 import {
-  TestAllTypesProto3,
-  TestAllTypesProto3_NestedMessage,
-} from "./gen/google/protobuf/test_messages_proto3_pb.js";
+  TestAllTypesProto3 as TS_TestAllTypesProto3,
+  TestAllTypesProto3_NestedMessage as TS_TestAllTypesProto3_NestedMessage,
+} from "./gen/ts/google/protobuf/test_messages_proto3_pb.js";
+import { TestAllTypesProto3 as JS_TestAllTypesProto3 } from "./gen/js/google/protobuf/test_messages_proto3_pb.js";
 import { testMT } from "./helpers.js";
 
-describe("constructor", function () {
-  test("takes partial message for oneof field", () => {
-    const m = new TestAllTypesProto3({
-      oneofField: {
-        case: "oneofNestedMessage",
-        value: {
-          corecursive: {
-            optionalInt32: 123,
-          },
-        },
-      },
-    });
-    expect(m.oneofField.case).toBe("oneofNestedMessage");
-    if (m.oneofField.case === "oneofNestedMessage") {
-      expect(m.oneofField.value.a).toBe(0);
-      expect(m.oneofField.value.corecursive).not.toBeUndefined();
-      expect(m.oneofField.value.corecursive?.optionalInt32).toBe(123);
-    }
-  });
-
-  test("takes partial message for map value", () => {
-    const m = new TestAllTypesProto3({
-      mapStringNestedMessage: {
-        key: {
-          corecursive: {
-            optionalInt32: 123,
-          },
-        },
-      },
-    });
-    expect(m.mapStringNestedMessage["key"]).toBeDefined();
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    if (m.mapStringNestedMessage["key"] !== undefined) {
-      expect(m.mapStringNestedMessage["key"].a).toBe(0);
-      expect(m.mapStringNestedMessage["key"].corecursive).not.toBeUndefined();
-      expect(m.mapStringNestedMessage["key"].corecursive?.optionalInt32).toBe(
-        123
-      );
-    }
-  });
-
-  testMT(TestAllTypesProto3, (messageType) => {
-    const t = new messageType({
-      optionalNestedMessage: {
-        a: 123,
-        corecursive: {
-          optionalNestedMessage: {
-            a: 456,
+describe("constructor takes partial message for oneof field", function () {
+  testMT(
+    { ts: TS_TestAllTypesProto3, js: JS_TestAllTypesProto3 },
+    (messageType) => {
+      const m = new messageType({
+        oneofField: {
+          case: "oneofNestedMessage",
+          value: {
             corecursive: {
-              optionalNestedMessage: {},
+              optionalInt32: 123,
             },
           },
         },
-      },
-      repeatedNestedMessage: [
-        {
-          a: 123,
-          corecursive: {
-            repeatedNestedMessage: [
-              {
-                a: 456,
-                corecursive: {
-                  repeatedNestedMessage: [{}],
-                },
-              },
-            ],
+      });
+      expect(m.oneofField.case).toBe("oneofNestedMessage");
+      if (m.oneofField.case === "oneofNestedMessage") {
+        expect(m.oneofField.value.a).toBe(0);
+        expect(m.oneofField.value.corecursive).not.toBeUndefined();
+        expect(m.oneofField.value.corecursive?.optionalInt32).toBe(123);
+      }
+    }
+  );
+});
+
+describe("constructor takes partial message for map value", function () {
+  testMT(
+    { ts: TS_TestAllTypesProto3, js: JS_TestAllTypesProto3 },
+    (messageType) => {
+      const m = new messageType({
+        mapStringNestedMessage: {
+          key: {
+            corecursive: {
+              optionalInt32: 123,
+            },
           },
         },
-      ],
-      mapStringString: {
-        a: "A",
-      },
-      oneofField: {
-        case: "oneofNestedMessage",
-        value: new TestAllTypesProto3_NestedMessage(),
-      },
-    });
-    expect(t.optionalNestedMessage?.a).toBe(123);
-    expect(t.optionalNestedMessage?.corecursive?.optionalNestedMessage?.a).toBe(
-      456
-    );
-    expect(
-      t.optionalNestedMessage?.corecursive?.optionalNestedMessage?.corecursive
-        ?.optionalNestedMessage?.a
-    ).toBe(0);
-    expect(t.repeatedNestedMessage.length).toBe(1);
-    expect(t.repeatedNestedMessage[0]?.a).toBe(123);
-    expect(
-      t.repeatedNestedMessage[0]?.corecursive?.repeatedNestedMessage[0]?.a
-    ).toBe(456);
-    expect(
-      t.repeatedNestedMessage[0]?.corecursive?.repeatedNestedMessage[0]
-        ?.corecursive?.repeatedNestedMessage[0]?.a
-    ).toBe(0);
-  });
+      });
+      expect(m.mapStringNestedMessage["key"]).toBeDefined();
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+      if (m.mapStringNestedMessage["key"] !== undefined) {
+        expect(m.mapStringNestedMessage["key"].a).toBe(0);
+        expect(m.mapStringNestedMessage["key"].corecursive).not.toBeUndefined();
+        expect(m.mapStringNestedMessage["key"].corecursive?.optionalInt32).toBe(
+          123
+        );
+      }
+    }
+  );
+  testMT(
+    { ts: TS_TestAllTypesProto3, js: JS_TestAllTypesProto3 },
+    (messageType) => {
+      const t = new messageType({
+        optionalNestedMessage: {
+          a: 123,
+          corecursive: {
+            optionalNestedMessage: {
+              a: 456,
+              corecursive: {
+                optionalNestedMessage: {},
+              },
+            },
+          },
+        },
+        repeatedNestedMessage: [
+          {
+            a: 123,
+            corecursive: {
+              repeatedNestedMessage: [
+                {
+                  a: 456,
+                  corecursive: {
+                    repeatedNestedMessage: [{}],
+                  },
+                },
+              ],
+            },
+          },
+        ],
+        mapStringString: {
+          a: "A",
+        },
+        oneofField: {
+          case: "oneofNestedMessage",
+          value: new TS_TestAllTypesProto3_NestedMessage(),
+        },
+      });
+      expect(t.optionalNestedMessage?.a).toBe(123);
+      expect(
+        t.optionalNestedMessage?.corecursive?.optionalNestedMessage?.a
+      ).toBe(456);
+      expect(
+        t.optionalNestedMessage?.corecursive?.optionalNestedMessage?.corecursive
+          ?.optionalNestedMessage?.a
+      ).toBe(0);
+      expect(t.repeatedNestedMessage.length).toBe(1);
+      expect(t.repeatedNestedMessage[0]?.a).toBe(123);
+      expect(
+        t.repeatedNestedMessage[0]?.corecursive?.repeatedNestedMessage[0]?.a
+      ).toBe(456);
+      expect(
+        t.repeatedNestedMessage[0]?.corecursive?.repeatedNestedMessage[0]
+          ?.corecursive?.repeatedNestedMessage[0]?.a
+      ).toBe(0);
+    }
+  );
 });
--- packages/protobuf-test/src/msg-message.test.ts
+++ packages/protobuf-test/src/msg-message.test.ts
@@ -13,69 +13,75 @@
 // limitations under the License.
 
 import type { JsonValue, PlainMessage } from "@bufbuild/protobuf";
-import { MessageFieldMessage } from "./gen/extra/msg-message_pb";
+import { MessageFieldMessage as TS_MessageFieldMessage } from "./gen/ts/extra/msg-message_pb.js";
+import { MessageFieldMessage as JS_MessageFieldMessage } from "./gen/js/extra/msg-message_pb.js";
 import { describeMT } from "./helpers.js";
 
-describeMT(MessageFieldMessage, (messageType) => {
-  const defaultFields: PlainMessage<MessageFieldMessage> = {
-    repeatedMessageField: [],
-  };
-  const defaultJson: JsonValue = {};
-  const exampleFields = {
-    messageField: { name: "test" },
-    repeatedMessageField: [{ name: "a" }, { name: "b" }],
-  };
-  const exampleJson: JsonValue = {
-    messageField: { name: "test" },
-    repeatedMessageField: [{ name: "a" }, { name: "b" }],
-  };
+describeMT(
+  { ts: TS_MessageFieldMessage, js: JS_MessageFieldMessage },
+  (messageType) => {
+    const defaultFields: PlainMessage<
+      TS_MessageFieldMessage | JS_MessageFieldMessage
+    > = {
+      repeatedMessageField: [],
+    };
+    const defaultJson: JsonValue = {};
+    const exampleFields = {
+      messageField: { name: "test" },
+      repeatedMessageField: [{ name: "a" }, { name: "b" }],
+    };
+    const exampleJson: JsonValue = {
+      messageField: { name: "test" },
+      repeatedMessageField: [{ name: "a" }, { name: "b" }],
+    };
 
-  test("has expected defaults", () => {
-    const got = { ...new messageType() };
-    expect(got).toStrictEqual(defaultFields);
-  });
-  test("defaults encodes to JSON", () => {
-    const got = new messageType().toJson();
-    expect(got).toStrictEqual(defaultJson);
-  });
-  test("defaults decodes from JSON", () => {
-    const got = messageType.fromJson(defaultJson);
-    expect(got.messageField?.name).toStrictEqual(
-      defaultFields.messageField?.name
-    );
-    expect(got.repeatedMessageField.length).toStrictEqual(
-      defaultFields.repeatedMessageField.length
-    );
-  });
-  test("example encodes to JSON", () => {
-    /* eslint-disable @typescript-eslint/no-explicit-any,@typescript-eslint/no-unsafe-member-access */
-    const got = new messageType(exampleFields).toJson();
-    expect((got as any).messageField?.name).toStrictEqual(
-      (exampleJson as any).messageField?.name
-    );
-    expect((got as any).repeatedMessageField.length).toStrictEqual(
-      (exampleJson as any).repeatedMessageField.length
-    );
-    expect((got as any).repeatedMessageField[0].name).toStrictEqual(
-      (exampleJson as any).repeatedMessageField[0].name
-    );
-    expect((got as any).repeatedMessageField[1].name).toStrictEqual(
-      (exampleJson as any).repeatedMessageField[1].name
-    );
-  });
-  test("example decodes from JSON", () => {
-    const got = messageType.fromJson(exampleJson);
-    expect(got.messageField?.name).toStrictEqual(
-      exampleFields.messageField.name
-    );
-    expect(got.repeatedMessageField.length).toStrictEqual(
-      exampleFields.repeatedMessageField.length
-    );
-    expect(got.repeatedMessageField[0].name).toStrictEqual(
-      exampleFields.repeatedMessageField[0].name
-    );
-    expect(got.repeatedMessageField[1].name).toStrictEqual(
-      exampleFields.repeatedMessageField[1].name
-    );
-  });
-});
+    test("has expected defaults", () => {
+      const got = { ...new messageType() };
+      expect(got).toStrictEqual(defaultFields);
+    });
+    test("defaults encodes to JSON", () => {
+      const got = new messageType().toJson();
+      expect(got).toStrictEqual(defaultJson);
+    });
+    test("defaults decodes from JSON", () => {
+      const got = messageType.fromJson(defaultJson);
+      expect(got.messageField?.name).toStrictEqual(
+        defaultFields.messageField?.name
+      );
+      expect(got.repeatedMessageField.length).toStrictEqual(
+        defaultFields.repeatedMessageField.length
+      );
+    });
+    test("example encodes to JSON", () => {
+      /* eslint-disable @typescript-eslint/no-explicit-any,@typescript-eslint/no-unsafe-member-access */
+      const got = new messageType(exampleFields).toJson();
+      expect((got as any).messageField?.name).toStrictEqual(
+        (exampleJson as any).messageField?.name
+      );
+      expect((got as any).repeatedMessageField.length).toStrictEqual(
+        (exampleJson as any).repeatedMessageField.length
+      );
+      expect((got as any).repeatedMessageField[0].name).toStrictEqual(
+        (exampleJson as any).repeatedMessageField[0].name
+      );
+      expect((got as any).repeatedMessageField[1].name).toStrictEqual(
+        (exampleJson as any).repeatedMessageField[1].name
+      );
+    });
+    test("example decodes from JSON", () => {
+      const got = messageType.fromJson(exampleJson);
+      expect(got.messageField?.name).toStrictEqual(
+        exampleFields.messageField.name
+      );
+      expect(got.repeatedMessageField.length).toStrictEqual(
+        exampleFields.repeatedMessageField.length
+      );
+      expect(got.repeatedMessageField[0].name).toStrictEqual(
+        exampleFields.repeatedMessageField[0].name
+      );
+      expect(got.repeatedMessageField[1].name).toStrictEqual(
+        exampleFields.repeatedMessageField[1].name
+      );
+    });
+  }
+);
