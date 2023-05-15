1a7282e93a126db91d3aadb1e5ec49030036d006
lukasIO <mail@lukasseiler.de>
2023-05-15 21:57:02 +0200

    prefere typeof BigInt check

--- packages/protobuf/src/proto-int64.ts
+++ packages/protobuf/src/proto-int64.ts
@@ -118,8 +118,7 @@ function makeInt64Support(): Int64Support {
   const dv = new DataView(new ArrayBuffer(8));
   // note that Safari 14 implements BigInt, but not the DataView methods
   const ok =
-    typeof globalThis !== "undefined" &&
-    (globalThis.BigInt as unknown) !== undefined &&
+    typeof BigInt !== "undefined" &&
     typeof dv.getBigInt64 === "function" &&
     typeof dv.getBigUint64 === "function" &&
     typeof dv.setBigInt64 === "function" &&
