4f34e990f9d18c2c1fbcc4117f8de123e86f41f6
Steve Ayers <sayers@buf.build>
2023-05-22 14:53:11 -0400

    Add additional tests to illustrate FieldMask serialization failures (#490)
    This adds additional unit tests to test the failure scenarios when
    serializing a `FieldMask` with invalid path names and when deserializing
    JSON with invalid field names in the string.
    
    ---------
    
    Co-authored-by: Timo Stamm <ts@timostamm.de>

--- packages/protoc-gen-es/src/typescript.ts
+++ packages/protoc-gen-es/src/typescript.ts
@@ -384,7 +384,7 @@ function generateWktMethods(schema: Schema, f: GeneratedFile, message: DescMessa
       f.print(`      text += "." + nanosStr;`)
       f.print("      if (this.", localName(ref.nanos), " < 0 && this.", localName(ref.seconds), " === ", protoInt64, ".zero) {");
       f.print(`          text = "-" + text;`);
-      f.print(`      }`);      
+      f.print(`      }`);
       f.print("    }")
       f.print(`    return text + "s";`)
       f.print("  }")
@@ -513,7 +513,7 @@ function generateWktMethods(schema: Schema, f: GeneratedFile, message: DescMessa
       f.print(`    }`)
       f.print(`    return this.`, localName(ref.paths), `.map(p => {`)
       f.print(`      if (p.match(/_[0-9]?_/g) || p.match(/[A-Z]/g)) {`)
-      f.print(`        throw new Error("cannot decode `, message.typeName, ` from JSON: lowerCamelCase of path name \\"" + p + "\\" is irreversible");`)
+      f.print(`        throw new Error("cannot encode `, message.typeName, ` to JSON: lowerCamelCase of path name \\"" + p + "\\" is irreversible");`)
       f.print(`      }`)
       f.print(`      return protoCamelCase(p);`)
       f.print(`    }).join(",");`)
