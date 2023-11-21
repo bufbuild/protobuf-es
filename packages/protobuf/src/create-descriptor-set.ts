ba8ed2871b9319e3c9902e96e374c768bd93fd38
Timo Stamm <ts@timostamm.de>
2023-11-21 17:09:55 +0100

    Revert Omit fix, will do a separate branch.

--- packages/protobuf/src/create-descriptor-set.ts
+++ packages/protobuf/src/create-descriptor-set.ts
@@ -640,7 +640,7 @@ function parseFileSyntax(
   syntax: string | undefined,
   edition: Edition | undefined,
 ) {
-  let e: Exclude<
+  let e: Omit<
     Edition,
     | Edition.EDITION_1_TEST_ONLY
     | Edition.EDITION_2_TEST_ONLY
