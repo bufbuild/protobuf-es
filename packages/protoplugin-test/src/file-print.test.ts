a200de3e3b381cc9d911b9e51da6789211cf8009
Timo Stamm <ts@timostamm.de>
2023-12-08 23:06:12 +0100

    Convert file-print.test.ts to use shared util

--- packages/protoplugin-test/src/file-print.test.ts
+++ packages/protoplugin-test/src/file-print.test.ts
@@ -13,15 +13,13 @@
 // limitations under the License.
 
 import { describe, expect, test } from "@jest/globals";
-import { CodeGeneratorRequest } from "@bufbuild/protobuf";
 import type { GeneratedFile, Schema } from "@bufbuild/protoplugin/ecmascript";
 import { createImportSymbol } from "@bufbuild/protoplugin/ecmascript";
-import { createEcmaScriptPlugin } from "@bufbuild/protoplugin";
-import { UpstreamProtobuf } from "upstream-protobuf";
+import { createTestPluginAndRun } from "./helpers";
 
 describe("file print", () => {
   test("should print bigint literals", async () => {
-    const lines = await testGenerate(`syntax="proto3";`, "target=ts", (f) => {
+    const lines = await testGenerate((f) => {
       f.print(BigInt(123));
       f.print(456n);
     });
@@ -34,14 +32,14 @@ describe("file print", () => {
   });
 
   test("should print number literals", async () => {
-    const lines = await testGenerate(`syntax="proto3";`, "target=ts", (f) => {
+    const lines = await testGenerate((f) => {
       f.print(123);
     });
     expect(lines).toStrictEqual(["123"]);
   });
 
   test("should print boolean literals", async () => {
-    const lines = await testGenerate(`syntax="proto3";`, "target=ts", (f) => {
+    const lines = await testGenerate((f) => {
       f.print(true);
       f.print(false);
     });
@@ -49,7 +47,7 @@ describe("file print", () => {
   });
 
   test("should print Uint8Array literals", async () => {
-    const lines = await testGenerate(`syntax="proto3";`, "target=ts", (f) => {
+    const lines = await testGenerate((f) => {
       f.print(new Uint8Array());
       f.print(new Uint8Array([0xde, 0xad, 0xbe, 0xef]));
     });
@@ -60,7 +58,7 @@ describe("file print", () => {
   });
 
   test("should print import symbol", async function () {
-    const lines = await testGenerate(`syntax="proto3";`, "target=ts", (f) => {
+    const lines = await testGenerate((f) => {
       const imp = createImportSymbol("Foo", "bar");
       f.print(imp);
     });
@@ -68,7 +66,7 @@ describe("file print", () => {
   });
 
   test("should print type-only import symbol", async function () {
-    const lines = await testGenerate(`syntax="proto3";`, "target=ts", (f) => {
+    const lines = await testGenerate((f) => {
       const imp = createImportSymbol("Foo", "bar");
       f.print(imp.toTypeOnly());
     });
@@ -76,7 +74,7 @@ describe("file print", () => {
   });
 
   test("should print import symbol used as type and value", async function () {
-    const lines = await testGenerate(`syntax="proto3";`, "target=ts", (f) => {
+    const lines = await testGenerate((f) => {
       const imp = createImportSymbol("Foo", "bar");
       f.print(imp);
       f.print(imp.toTypeOnly());
@@ -90,7 +88,7 @@ describe("file print", () => {
   });
 
   test("should print only one import for the same symbol", async function () {
-    const lines = await testGenerate(`syntax="proto3";`, "target=ts", (f) => {
+    const lines = await testGenerate((f) => {
       const imp = createImportSymbol("Foo", "bar");
       const imp2 = createImportSymbol("Foo", "bar");
       f.print(imp);
@@ -105,7 +103,7 @@ describe("file print", () => {
   });
 
   test("should escape clashing import symbols", async function () {
-    const lines = await testGenerate(`syntax="proto3";`, "target=ts", (f) => {
+    const lines = await testGenerate((f) => {
       const imp = createImportSymbol("Foo", "a");
       const imp2 = createImportSymbol("Foo", "b");
       f.print(imp);
@@ -121,16 +119,17 @@ describe("file print", () => {
   });
 
   test("should escape clashing import symbols with commonjs", async function () {
-    const lines = await testGenerate(
-      `syntax="proto3";`,
-      "target=js,js_import_style=legacy_commonjs",
-      (f) => {
+    const lines = await createTestPluginAndRun({
+      proto: `syntax="proto3";`,
+      parameter: "target=js,js_import_style=legacy_commonjs",
+      generateAny(f) {
         const imp = createImportSymbol("Foo", "a");
         const imp2 = createImportSymbol("Foo", "b");
         f.print(imp);
         f.print(imp2);
       },
-    );
+      returnLinesOfFirstFile: true,
+    });
     expect(lines).toStrictEqual([
       `"use strict";`,
       `Object.defineProperty(exports, "__esModule", { value: true });`,
@@ -144,13 +143,9 @@ describe("file print", () => {
   });
 
   test("should print runtime imports", async () => {
-    const lines = await testGenerate(
-      `syntax="proto3";`,
-      "target=ts",
-      (f, schema) => {
-        f.print(schema.runtime.ScalarType, ".INT32");
-      },
-    );
+    const lines = await testGenerate((f, schema) => {
+      f.print(schema.runtime.ScalarType, ".INT32");
+    });
     expect(lines).toStrictEqual([
       'import { ScalarType } from "@bufbuild/protobuf";',
       "",
@@ -159,15 +154,17 @@ describe("file print", () => {
   });
 
   test("should print descriptor", async function () {
-    const lines = await testGenerate(
-      `syntax="proto3";
-            message Person {}
-            `,
-      "target=ts",
-      (f, schema) => {
+    const lines = await createTestPluginAndRun({
+      proto: `
+          syntax="proto3";
+          message Person {}
+          `,
+      parameter: "target=ts",
+      generateAny(f, schema) {
         f.print(schema.files[0].messages[0], ".typeName");
       },
-    );
+      returnLinesOfFirstFile: true,
+    });
     expect(lines).toStrictEqual([
       'import { Person } from "./x_pb.js";',
       "",
@@ -176,7 +173,7 @@ describe("file print", () => {
   });
 
   test("should print empty lines", async () => {
-    const lines = await testGenerate(`syntax="proto3";`, "target=ts", (f) => {
+    const lines = await testGenerate((f) => {
       f.print(" ");
       f.print("");
       f.print();
@@ -185,14 +182,14 @@ describe("file print", () => {
   });
 
   test("should print multiple printables", async () => {
-    const lines = await testGenerate(`syntax="proto3";`, "target=ts", (f) => {
+    const lines = await testGenerate((f) => {
       f.print("a", "b", "c", 1, " ", createImportSymbol("Foo", "bar"));
     });
     expect(lines).toStrictEqual([`import { Foo } from "bar";`, "", "abc1 Foo"]);
   });
 
   test("should print nested printables", async () => {
-    const lines = await testGenerate(`syntax="proto3";`, "target=ts", (f) => {
+    const lines = await testGenerate((f) => {
       // prettier-ignore
       f.print("a", ["b", ["c", "d", [1, " ", createImportSymbol("Foo", "bar")]]]);
     });
@@ -205,21 +202,21 @@ describe("file print", () => {
 
   describe("with tagged template literals", () => {
     test("should print empty lines", async () => {
-      const lines = await testGenerate(`syntax="proto3";`, "target=ts", (f) => {
+      const lines = await testGenerate((f) => {
         f.print` `;
         f.print``;
       });
       expect(lines).toStrictEqual([" ", ""]);
     });
     test("should print import symbol", async () => {
-      const lines = await testGenerate(`syntax="proto3";`, "target=ts", (f) => {
+      const lines = await testGenerate((f) => {
         const imp = createImportSymbol("Foo", "bar");
         f.print`${imp}`;
       });
       expect(lines).toStrictEqual(['import { Foo } from "bar";', "", "Foo"]);
     });
     test("should print real-world import use case", async () => {
-      const lines = await testGenerate(`syntax="proto3";`, "target=ts", (f) => {
+      const lines = await testGenerate((f) => {
         const Foo = createImportSymbol("Foo", "bar");
         f.print`export function foo(): ${Foo.toTypeOnly()} {
   return new ${Foo}();
@@ -234,7 +231,7 @@ describe("file print", () => {
       ]);
     });
     test("should print multiple printables", async () => {
-      const lines = await testGenerate(`syntax="proto3";`, "target=ts", (f) => {
+      const lines = await testGenerate((f) => {
         f.print`${"a"}${"b"}${"c"}${1} ${createImportSymbol("Foo", "bar")}`;
       });
       expect(lines).toStrictEqual([
@@ -244,7 +241,7 @@ describe("file print", () => {
       ]);
     });
     test("should print nested printables", async () => {
-      const lines = await testGenerate(`syntax="proto3";`, "target=ts", (f) => {
+      const lines = await testGenerate((f) => {
         // prettier-ignore
         f.print`${"a"}${["b", ["c", "d", [1, " ", createImportSymbol("Foo", "bar")]]]}`;
       });
@@ -256,37 +253,12 @@ describe("file print", () => {
     });
   });
 
-  async function testGenerate(
-    proto: string,
-    parameter: string,
-    gen: (f: GeneratedFile, schema: Schema) => void,
-  ) {
-    const plugin = createEcmaScriptPlugin({
-      name: "test",
-      version: "v1",
-      generateTs: generateAny,
-      generateJs: generateAny,
-      generateDts: generateAny,
+  async function testGenerate(opt: (f: GeneratedFile, schema: Schema) => void) {
+    return createTestPluginAndRun({
+      proto: `syntax="proto3";`,
+      parameter: "target=ts",
+      generateAny: opt,
+      returnLinesOfFirstFile: true,
     });
-
-    function generateAny(schema: Schema) {
-      gen(schema.generateFile("test.ts"), schema);
-    }
-
-    const upstream = new UpstreamProtobuf();
-    const protoFiles = {
-      "x.proto": proto,
-    };
-    const reqBytes = await upstream.createCodeGeneratorRequest(protoFiles, {
-      parameter,
-    });
-    const req = CodeGeneratorRequest.fromBinary(reqBytes);
-    const res = plugin.run(req);
-    expect(res.file.length).toBeGreaterThanOrEqual(1);
-    let content = res.file[0]?.content ?? "";
-    if (content.endsWith("\n")) {
-      content = content.slice(0, -1); // trim final newline so we don't return an extra line
-    }
-    return content.split("\n");
   }
 });
