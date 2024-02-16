8b3e72c06817f6edb9ac47b193db8e54ea84212f
Timo Stamm <ts@timostamm.de>
2024-02-16 14:52:59 +0100

    Change makeMessageType() to also use the prototype chain for proto2 optional

--- packages/protobuf/src/private/message-type.ts
+++ packages/protobuf/src/private/message-type.ts
@@ -19,8 +19,8 @@ import type { JsonReadOptions, JsonValue } from "../json-format.js";
 import type { MessageType } from "../message-type.js";
 import type { BinaryReadOptions } from "../binary-format.js";
 import type { ProtoRuntime } from "./proto-runtime.js";
-import { fieldUsesPrototype } from "./reflect.js";
 import { scalarZeroValue } from "./scalars.js";
+import type { FieldInfo } from "../field";
 
 /**
  * Create a new message type using the given runtime.
@@ -44,12 +44,12 @@ export function makeMessageType<T extends Message<T> = AnyMessage>(
 ): MessageType<T> {
   const localName =
     opt?.localName ?? typeName.substring(typeName.lastIndexOf(".") + 1);
-  let prototypeFieldsAdded = false;
+  let prototypeFieldsNeeded = runtime.syntax == "proto2";
   const type = {
     [localName]: function (this: T, data?: PartialMessage<T>) {
-      if (!prototypeFieldsAdded) {
+      if (prototypeFieldsNeeded) {
         addPrototypeFields(type);
-        prototypeFieldsAdded = true;
+        prototypeFieldsNeeded = false;
       }
       runtime.util.initFields(this);
       runtime.util.initPartial(data, this);
@@ -95,4 +95,16 @@ function addPrototypeFields(ctor: MessageType) {
     }
     (ctor.prototype as Record<string, unknown>)[field.localName] = value;
   }
+}
+
+// Behavior must match with the counterpart in @bufbuild/protoc-gen-es
+function fieldUsesPrototype(field: FieldInfo): boolean {
+  if (field.repeated || field.oneof) {
+    return false;
+  }
+  if (field.kind != "scalar" && field.kind != "enum") {
+    return false;
+  }
+  // proto2 singular scalar and enum fields use an initial value on the prototype chain
+  return true;
 }
