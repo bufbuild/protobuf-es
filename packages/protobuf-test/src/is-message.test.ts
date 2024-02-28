cac27d88add1943e3c8c1cccf1fb864a5b23216f
Steve Ayers <sayers@buf.build>
2024-02-28 10:40:19 -0500

    Tests

---
+++ packages/protobuf-test/src/is-message.test.ts
@@ -0,0 +1,33 @@
+// Copyright 2021-2024 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { describe, expect, test } from "@jest/globals";
+import { User } from "./gen/ts/extra/example_pb.js";
+import { isMessage, Message } from "@bufbuild/protobuf";
+
+describe("isMessage", () => {
+  test("Message", () => {
+    const msg = new Message();
+
+    expect(isMessage(msg)).toBeTruthy();
+  });
+  test("subclass of Message", () => {
+    const user = new User({
+      firstName: "Homer",
+      lastName: "Simpson",
+    });
+
+    expect(isMessage(user, User)).toBeTruthy();
+  });
+});
