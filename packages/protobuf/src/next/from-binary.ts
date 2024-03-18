689daf333ed5761b4011046cc4f90220ffdf5ceb
Sri Krishna <93153132+srikrsna-buf@users.noreply.github.com>
2024-03-18 19:34:13 +0530

    Add `toBinary` and `fromBinary` functions (#751)
    Add `toBinary` and `fromBinary` functions to serialize to and from
    protobuf encoded bytes. Added compatibility tests with v1 for now.

---
+++ packages/protobuf/src/next/from-binary.ts
@@ -0,0 +1,277 @@
+// Copyright 2021-2024 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import type { BinaryReadOptions } from "../binary-format.js";
+import type { DescField, DescMessage } from "../descriptor-set.js";
+import type { MessageShape } from "./types.js";
+import type { ReflectMessage } from "./reflect/reflect.js";
+import type { IBinaryReader } from "../binary-encoding.js";
+import {
+  LongType,
+  scalarZeroValue,
+  type ScalarValue,
+} from "./reflect/scalar.js";
+import { reflect } from "./reflect/reflect.js";
+import { create } from "./create.js";
+import { BinaryReader, WireType } from "../binary-encoding.js";
+import { ScalarType } from "./reflect/scalar.js";
+import {
+  FeatureSet_MessageEncoding,
+  FieldDescriptorProto_Type,
+} from "../google/protobuf/descriptor_pb.js";
+
+// Default options for parsing binary data.
+const readDefaults: Readonly<BinaryReadOptions> = {
+  readUnknownFields: true,
+  readerFactory: (bytes) => new BinaryReader(bytes),
+};
+
+function makeReadOptions(
+  options?: Partial<BinaryReadOptions>,
+): Readonly<BinaryReadOptions> {
+  return options ? { ...readDefaults, ...options } : readDefaults;
+}
+
+export function fromBinary<Desc extends DescMessage>(
+  desc: Desc,
+  bytes: Uint8Array,
+  options?: Partial<BinaryReadOptions>,
+): MessageShape<Desc> {
+  options = makeReadOptions(options);
+  return readMessage(
+    desc,
+    options.readerFactory!(bytes),
+    bytes.byteLength,
+    options as BinaryReadOptions,
+  );
+}
+
+// TODO: Improve the function signature, we got most it from v1.
+function readMessage<Desc extends DescMessage>(
+  desc: Desc,
+  reader: IBinaryReader,
+  lengthOrEndTagFieldNo: number,
+  options: BinaryReadOptions,
+  delimitedMessageEncoding?: boolean,
+): MessageShape<Desc> {
+  const message = create(desc);
+  const rMessage = reflect(message);
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  const end = delimitedMessageEncoding
+    ? reader.len
+    : reader.pos + lengthOrEndTagFieldNo;
+  let fieldNo: number | undefined, wireType: WireType | undefined;
+  while (reader.pos < end) {
+    [fieldNo, wireType] = reader.tag();
+    if (wireType == WireType.EndGroup) {
+      break;
+    }
+    const field = rMessage.findNumber(fieldNo);
+    if (!field) {
+      reader.skip(wireType);
+      // TODO: read unknown fields
+      continue;
+    }
+    readField(rMessage, reader, field, wireType, options);
+  }
+  if (
+    delimitedMessageEncoding && // eslint-disable-line @typescript-eslint/strict-boolean-expressions
+    (wireType != WireType.EndGroup || fieldNo !== lengthOrEndTagFieldNo)
+  ) {
+    throw new Error(`invalid end group tag`);
+  }
+  return message;
+}
+
+function readField(
+  message: ReflectMessage,
+  reader: IBinaryReader,
+  field: DescField,
+  wireType: WireType,
+  options: BinaryReadOptions,
+) {
+  switch (field.fieldKind) {
+    case "scalar":
+      message.set(field, readScalar(reader, field.scalar, field.longType));
+      break;
+    case "enum":
+      message.set(field, readScalar(reader, ScalarType.INT32) as number);
+      break;
+    case "message":
+      message.set(field, readMessageField(reader, options, field));
+      break;
+    case "list":
+      readRepeatedField(message, reader, options, field, wireType);
+      break;
+    case "map":
+      // eslint-disable-next-line no-case-declarations
+      const [key, val] = readMapEntry(field, reader, options);
+      message.setMapEntry(field, key, val);
+      break;
+  }
+}
+
+// Read a map field, expecting key field = 1, value field = 2
+//
+// TODO: Support edition features
+function readMapEntry(
+  field: DescField & { fieldKind: "map" },
+  reader: IBinaryReader,
+  options: BinaryReadOptions,
+): [string | number, ScalarValue | MessageShape<DescMessage>] {
+  const length = reader.uint32(),
+    end = reader.pos + length;
+  let key: ScalarValue | undefined,
+    val: ScalarValue | MessageShape<DescMessage> | undefined;
+  while (reader.pos < end) {
+    const [fieldNo] = reader.tag();
+    switch (fieldNo) {
+      case 1:
+        key = readScalar(reader, field.mapKey);
+        break;
+      case 2:
+        switch (field.mapKind) {
+          case "scalar":
+            val = readScalar(reader, field.scalar);
+            break;
+          case "enum":
+            val = reader.int32();
+            break;
+          case "message":
+            val = readMessageField(reader, options, field);
+            break;
+        }
+        break;
+    }
+  }
+  if (key === undefined) {
+    key = scalarZeroValue(field.mapKey, LongType.BIGINT);
+  }
+  if (typeof key != "string" && typeof key != "number") {
+    key = key.toString();
+  }
+  if (val === undefined) {
+    switch (field.mapKind) {
+      case "scalar":
+        val = scalarZeroValue(field.scalar, LongType.BIGINT);
+        break;
+      case "enum":
+        val = field.enum.values[0].number;
+        break;
+      case "message":
+        val = create(field.message);
+        break;
+    }
+  }
+  return [key, val];
+}
+
+function readRepeatedField(
+  message: ReflectMessage,
+  reader: IBinaryReader,
+  options: BinaryReadOptions,
+  field: DescField & { fieldKind: "list" },
+  wireType: WireType,
+) {
+  if (field.listKind === "message") {
+    message.addListItem(
+      field,
+      readMessageField(reader, options, field) as never, // TODO: Investigate the type issue.
+    );
+    return;
+  }
+  const scalarType = field.scalar ?? ScalarType.INT32;
+  const longType = field.listKind == "scalar" ? field.longType : undefined;
+  const packed =
+    wireType == WireType.LengthDelimited &&
+    scalarType != ScalarType.STRING &&
+    scalarType != ScalarType.BYTES;
+  if (!packed) {
+    message.addListItem(field, readScalar(reader, scalarType, longType));
+    return;
+  }
+  const arr = [] as ScalarValue[];
+  const e = reader.uint32() + reader.pos;
+  while (reader.pos < e) {
+    arr.push(readScalar(reader, scalarType, longType));
+  }
+  message.set(field, arr as number[]); // TODO: Investigate the type
+}
+
+function readMessageField<Desc extends DescMessage>(
+  reader: IBinaryReader,
+  options: BinaryReadOptions,
+  field: Pick<DescField, "number" | "proto" | "getFeatures"> & {
+    message: Desc;
+  },
+): MessageShape<Desc> {
+  const delimited =
+    field.proto.type === FieldDescriptorProto_Type.GROUP ||
+    field.getFeatures().messageEncoding ===
+      FeatureSet_MessageEncoding.DELIMITED;
+  return readMessage(
+    field.message,
+    reader,
+    delimited ? field.number : reader.uint32(), // eslint-disable-line @typescript-eslint/strict-boolean-expressions
+    options,
+    delimited,
+  );
+}
+
+function readScalar(
+  reader: IBinaryReader,
+  type: ScalarType,
+  longType?: LongType,
+): ScalarValue {
+  let v = readScalarValue(reader, type);
+  if (longType === LongType.STRING) {
+    v = typeof v == "bigint" ? v.toString() : v;
+  }
+  return v;
+}
+
+function readScalarValue(reader: IBinaryReader, type: ScalarType): ScalarValue {
+  switch (type) {
+    case ScalarType.STRING:
+      return reader.string();
+    case ScalarType.BOOL:
+      return reader.bool();
+    case ScalarType.DOUBLE:
+      return reader.double();
+    case ScalarType.FLOAT:
+      return reader.float();
+    case ScalarType.INT32:
+      return reader.int32();
+    case ScalarType.INT64:
+      return reader.int64();
+    case ScalarType.UINT64:
+      return reader.uint64();
+    case ScalarType.FIXED64:
+      return reader.fixed64();
+    case ScalarType.BYTES:
+      return reader.bytes();
+    case ScalarType.FIXED32:
+      return reader.fixed32();
+    case ScalarType.SFIXED32:
+      return reader.sfixed32();
+    case ScalarType.SFIXED64:
+      return reader.sfixed64();
+    case ScalarType.SINT64:
+      return reader.sint64();
+    case ScalarType.UINT32:
+      return reader.uint32();
+    case ScalarType.SINT32:
+      return reader.sint32();
+  }
+}
---
+++ packages/protobuf/src/next/to-binary.ts
@@ -0,0 +1,271 @@
+// Copyright 2021-2024 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import type { ReflectMessage } from "./reflect/reflect.js";
+import type { Message } from "./types.js";
+import type { BinaryWriteOptions } from "../binary-format.js";
+import { reflect } from "./reflect/reflect.js";
+import {
+  BinaryWriter,
+  WireType,
+  type IBinaryWriter,
+} from "../binary-encoding.js";
+import {
+  FeatureSet_FieldPresence,
+  FeatureSet_MessageEncoding,
+  FieldDescriptorProto_Label,
+  FieldDescriptorProto_Type,
+} from "../google/protobuf/descriptor_pb.js";
+import { ScalarType, type ScalarValue } from "./reflect/scalar.js";
+import type { DescField } from "../descriptor-set.js";
+
+// Default options for serializing binary data.
+const writeDefaults: Readonly<BinaryWriteOptions> = {
+  writeUnknownFields: true,
+  writerFactory: () => new BinaryWriter(),
+};
+
+function makeWriteOptions(
+  options?: Partial<BinaryWriteOptions>,
+): Readonly<BinaryWriteOptions> {
+  return options ? { ...writeDefaults, ...options } : writeDefaults;
+}
+
+export function toBinary<T extends Message>(
+  message: T,
+  options?: Partial<BinaryWriteOptions>,
+): Uint8Array {
+  return reflectToBinary(reflect(message), makeWriteOptions(options));
+}
+
+function reflectToBinary(
+  msg: ReflectMessage,
+  opts: BinaryWriteOptions,
+): Uint8Array {
+  const w = opts.writerFactory();
+  for (const f of msg.fields) {
+    if (!msg.isSet(f)) {
+      if (
+        f.proto.label === FieldDescriptorProto_Label.REQUIRED ||
+        f.getFeatures().fieldPresence ==
+          FeatureSet_FieldPresence.LEGACY_REQUIRED
+      ) {
+        throw new Error(
+          `cannot encode field ${msg.desc.typeName}.${f.name} to binary: required field not set`,
+        );
+      }
+      continue;
+    }
+    switch (f.fieldKind) {
+      case "scalar":
+      case "enum":
+        writeScalar(w, f.scalar ?? ScalarType.INT32, f.number, msg.get(f));
+        break;
+      case "list":
+        writeListField(w, opts, f, msg.get(f));
+        break;
+      case "message":
+        writeMessageField(w, opts, f, msg.get(f) as Message);
+        break;
+      case "map":
+        for (const [key, val] of Object.entries(msg.get(f))) {
+          writeMapEntry(w, opts, f, key, val);
+        }
+        break;
+    }
+  }
+  return w.finish();
+}
+
+function writeScalar(
+  writer: IBinaryWriter,
+  scalarType: ScalarType,
+  fieldNo: number,
+  value: unknown,
+) {
+  writeScalarValue(
+    writer.tag(fieldNo, writeTypeOfScalar(scalarType)),
+    scalarType,
+    value as ScalarValue,
+  );
+}
+
+function writeMessageField(
+  writer: IBinaryWriter,
+  opts: BinaryWriteOptions,
+  field: DescField &
+    ({ fieldKind: "message" } | { fieldKind: "list"; listKind: "message" }),
+  message: Message,
+) {
+  const rm = reflectToBinary(reflect(message), opts);
+  if (
+    field.proto.type === FieldDescriptorProto_Type.GROUP ||
+    field.getFeatures().messageEncoding === FeatureSet_MessageEncoding.DELIMITED
+  ) {
+    writer
+      .tag(field.number, WireType.StartGroup)
+      .raw(rm) // TODO: Optimise this to not create a new writer.
+      .tag(field.number, WireType.EndGroup);
+  } else {
+    writer.tag(field.number, WireType.LengthDelimited).bytes(rm);
+  }
+}
+
+function writeListField(
+  writer: IBinaryWriter,
+  opts: BinaryWriteOptions,
+  field: DescField & { fieldKind: "list" },
+  value: readonly unknown[],
+) {
+  if (field.listKind == "message") {
+    for (const item of value) {
+      writeMessageField(writer, opts, field, item as Message);
+    }
+    return;
+  }
+  const scalarType = field.scalar ?? ScalarType.INT32;
+  if (field.packed) {
+    if (!value.length) {
+      return;
+    }
+    writer.tag(field.number, WireType.LengthDelimited).fork();
+    for (let i = 0; i < value.length; i++) {
+      writeScalarValue(writer, scalarType, value[i] as ScalarValue);
+    }
+    writer.join();
+    return;
+  }
+  for (const item of value) {
+    writeScalar(writer, scalarType, field.number, item);
+  }
+}
+
+function writeMapEntry(
+  writer: IBinaryWriter,
+  opts: BinaryWriteOptions,
+  field: DescField & { fieldKind: "map" },
+  key: string,
+  value: unknown,
+) {
+  writer.tag(field.number, WireType.LengthDelimited);
+  writer.fork();
+  // javascript only allows number or string for object properties
+  // we convert from our representation to the protobuf type
+  let keyValue: ScalarValue;
+  switch (field.mapKey) {
+    case ScalarType.INT32:
+    case ScalarType.FIXED32:
+    case ScalarType.UINT32:
+    case ScalarType.SFIXED32:
+    case ScalarType.SINT32:
+      keyValue = Number.parseInt(key);
+      break;
+    case ScalarType.BOOL:
+      keyValue = key == "true";
+      break;
+    default:
+      keyValue = key;
+      break;
+  }
+
+  // write key, expecting key field number = 1
+  writeScalar(writer, field.mapKey, 1, keyValue);
+
+  // write value, expecting value field number = 2
+  switch (field.mapKind) {
+    case "scalar":
+    case "enum":
+      writeScalar(writer, field.scalar ?? ScalarType.INT32, 2, value);
+      break;
+    case "message":
+      writer
+        .tag(2, WireType.LengthDelimited)
+        .bytes(reflectToBinary(reflect(value as Message), opts));
+      break;
+  }
+
+  writer.join();
+}
+
+function writeScalarValue(
+  writer: IBinaryWriter,
+  type: ScalarType,
+  value: ScalarValue,
+) {
+  switch (type) {
+    case ScalarType.STRING:
+      writer.string(value as string);
+      break;
+    case ScalarType.BOOL:
+      writer.bool(value as boolean);
+      break;
+    case ScalarType.DOUBLE:
+      writer.double(value as number);
+      break;
+    case ScalarType.FLOAT:
+      writer.float(value as number);
+      break;
+    case ScalarType.INT32:
+      writer.int32(value as number);
+      break;
+    case ScalarType.INT64:
+      writer.int64(value as number);
+      break;
+    case ScalarType.UINT64:
+      writer.uint64(value as number);
+      break;
+    case ScalarType.FIXED64:
+      writer.fixed64(value as number);
+      break;
+    case ScalarType.BYTES:
+      writer.bytes(value as Uint8Array);
+      break;
+    case ScalarType.FIXED32:
+      writer.fixed32(value as number);
+      break;
+    case ScalarType.SFIXED32:
+      writer.sfixed32(value as number);
+      break;
+    case ScalarType.SFIXED64:
+      writer.sfixed64(value as number);
+      break;
+    case ScalarType.SINT64:
+      writer.sint64(value as number);
+      break;
+    case ScalarType.UINT32:
+      writer.uint32(value as number);
+      break;
+    case ScalarType.SINT32:
+      writer.sint32(value as number);
+      break;
+  }
+}
+
+function writeTypeOfScalar(type: ScalarType): WireType {
+  switch (type) {
+    case ScalarType.BYTES:
+    case ScalarType.STRING:
+      return WireType.LengthDelimited;
+    case ScalarType.DOUBLE:
+    case ScalarType.FIXED64:
+    case ScalarType.SFIXED64:
+      return WireType.Bit64;
+    case ScalarType.FIXED32:
+    case ScalarType.SFIXED32:
+    case ScalarType.FLOAT:
+      return WireType.Bit32;
+    default:
+      return WireType.Varint;
+  }
+}
