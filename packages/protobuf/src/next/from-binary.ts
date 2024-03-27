bufbuild/protobuf-es

commit 7054ed7582e384acf4b653820d10bcc7ec991e60

Update reflect
This adds two new types to the reflect API: ReflectList and ReflectMap. The API now always returns messages as ReflectMessage, list fields as ReflectList, and map fields as ReflectMap. Note that accessing an unset message fields returns a ReflectMessage with a new zero-message.

The methods ReflectMessage.addListItem and ReflectMessage.setMapEntry are kept for now. We can decide whether it's worth to keep them for the hot path when we take a look at performance.

This also updates create() to fully support the "init" argument.

packages/protobuf/src/next/from-binary.ts
@@ -15,14 +15,14 @@
 import type { BinaryReadOptions } from "../binary-format.js";
 import type { DescField, DescMessage } from "../descriptor-set.js";
 import type { MessageShape } from "./types.js";
-import type { ReflectMessage } from "./reflect/reflect.js";
+import type { ReflectMessage } from "./reflect/index.js";
 import type { IBinaryReader } from "../binary-encoding.js";
 import {
   LongType,
   scalarZeroValue,
   type ScalarValue,
 } from "./reflect/scalar.js";
-import { reflect } from "./reflect/reflect.js";
+import { reflect, reflectMessage } from "./reflect/reflect.js";
 import { create } from "./create.js";
 import { BinaryReader, WireType } from "../binary-encoding.js";
 import { ScalarType } from "./reflect/scalar.js";
@@ -88,7 +88,7 @@ function readMessage(
   lengthOrEndTagFieldNo: number,
   options: BinaryReadOptions,
   delimitedMessageEncoding?: boolean,
-) {
+): void {
   // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
   const end = delimitedMessageEncoding
     ? reader.len
@@ -159,11 +159,11 @@ function readMapEntry(
   field: DescField & { fieldKind: "map" },
   reader: IBinaryReader,
   options: BinaryReadOptions,
-): [string | number, ScalarValue | MessageShape<DescMessage>] {
+): [string | number, ScalarValue | ReflectMessage] {
   const length = reader.uint32(),
     end = reader.pos + length;
   let key: ScalarValue | undefined,
-    val: ScalarValue | MessageShape<DescMessage> | undefined;
+    val: ScalarValue | ReflectMessage | undefined;
   while (reader.pos < end) {
     const [fieldNo] = reader.tag();
     switch (fieldNo) {
@@ -200,7 +200,7 @@ function readMapEntry(
         val = field.enum.values[0].number;
         break;
       case "message":
-        val = create(field.message);
+        val = reflectMessage(field.message);
         break;
     }
   }
@@ -243,15 +243,15 @@ function readMessageField<Desc extends DescMessage>(
   field: Pick<DescField, "number" | "proto" | "getFeatures"> & {
     message: Desc;
   },
-  mergeMessage?: MessageShape<Desc>,
-): MessageShape<Desc> {
+  mergeMessage?: ReflectMessage,
+): ReflectMessage {
   const delimited =
     field.proto.type === FieldDescriptorProto_Type.GROUP ||
     field.getFeatures().messageEncoding ===
       FeatureSet_MessageEncoding.DELIMITED;
-  const message = mergeMessage ?? create(field.message);
+  const message = mergeMessage ?? reflectMessage(field.message);
   readMessage(
-    reflect(message),
+    message,
     reader,
     delimited ? field.number : reader.uint32(), // eslint-disable-line @typescript-eslint/strict-boolean-expressions
     options,

packages/protobuf-test/src/next/helpers.ts
@@ -16,7 +16,9 @@ import { describe, test } from "@jest/globals";
 import type { DescMessage } from "@bufbuild/protobuf";
 import { FileDescriptorSet } from "@bufbuild/protobuf";
 import { UpstreamProtobuf } from "upstream-protobuf";
-import { createDescFileSet } from "@bufbuild/protobuf/next/reflect";
+import { createDescFileSet, localName } from "@bufbuild/protobuf/next/reflect";
+import * as proto3_ts from "../gen/ts/extra/proto3_pbv2.js";
+import type { DescField } from "@bufbuild/protobuf";
 
 export function describeGenerated<Desc extends DescMessage>(
   ts: Desc,
@@ -73,4 +75,41 @@ export async function compileMessage(proto: string): Promise<DescMessage> {
     throw new Error("missing message in input.proto");
   }
   return file.messages[0];
+}
+
+export function getFieldByLocalName(desc: DescMessage, name: string): DescField;
+export function getFieldByLocalName(
+  desc: DescMessage,
+  name: string,
+  fieldKind: "message",
+): DescField & { fieldKind: "message" };
+export function getFieldByLocalName(
+  desc: DescMessage,
+  name: string,
+  fieldKind: "list",
+): DescField & { fieldKind: "list" };
+export function getFieldByLocalName(
+  desc: DescMessage,
+  name: string,
+  fieldKind: "map",
+): DescField & { fieldKind: "map" };
+export function getFieldByLocalName(
+  desc: DescMessage,
+  name: string,
+  fieldKind?: string,
+): DescField {
+  const field = proto3_ts.Proto3MessageDesc.fields.find(
+    (f) => localName(f) === name,
+  );
+  if (!field) {
+    throw new Error(`getFieldByLocalName: ${name} not found`);
+  }
+  if (fieldKind !== undefined) {
+    if (field.fieldKind != fieldKind) {
+      throw new Error(
+        `getFieldByLocalName: ${name} is not a ${fieldKind} field`,
+      );
+    }
+  }
+  return field;
 }

packages/protobuf-test/src/next/reflect/reflect-list.test.ts
@@ -1,0 +1,231 @@
+// Copyright 2021-2024 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { describe, expect, test } from "@jest/globals";
+import {
+  isReflectList,
+  reflectList,
+  reflectMessage,
+} from "@bufbuild/protobuf/next/reflect";
+import { getFieldByLocalName } from "../helpers.js";
+import * as proto3_ts from "../../gen/ts/extra/proto3_pbv2.js";
+import { protoInt64 } from "@bufbuild/protobuf";
+import { UserDesc } from "../../gen/ts/extra/example_pbv2.js";
+
+describe("reflectList()", () => {
+  test("creates ReflectList", () => {
+    const f = getFieldByLocalName(
+      proto3_ts.Proto3MessageDesc,
+      "repeatedStringField",
+      "list",
+    );
+    const list = reflectList(f);
+    expect(typeof list.field).toBe("function");
+    expect(isReflectList(list)).toBe(true);
+  });
+  test("creates ReflectList with unsafe input", () => {
+    const f = getFieldByLocalName(
+      proto3_ts.Proto3MessageDesc,
+      "repeatedStringField",
+      "list",
+    );
+    const list = reflectList(f, [1, 2, 3]);
+    expect(isReflectList(list)).toBe(true);
+  });
+});
+
+describe("ReflectList", () => {
+  const repeatedStringField = getFieldByLocalName(
+    proto3_ts.Proto3MessageDesc,
+    "repeatedStringField",
+    "list",
+  );
+  const repeatedInt64Field = getFieldByLocalName(
+    proto3_ts.Proto3MessageDesc,
+    "repeatedInt64Field",
+    "list",
+  );
+  const repeatedInt64JsStringField = getFieldByLocalName(
+    proto3_ts.Proto3MessageDesc,
+    "repeatedInt64JsStringField",
+    "list",
+  );
+  const repeatedMessageField = getFieldByLocalName(
+    proto3_ts.Proto3MessageDesc,
+    "repeatedMessageField",
+    "list",
+  );
+  const n0 = protoInt64.zero;
+  const n1 = protoInt64.parse(1);
+  const n2 = protoInt64.parse(2);
+  const n3 = protoInt64.parse(3);
+  describe("field()", () => {
+    test("returns the field", () => {
+      const list = reflectList(repeatedStringField, []);
+      expect(list.field()).toBe(repeatedStringField);
+    });
+  });
+  describe("size", () => {
+    test("returns size of the list", () => {
+      const a = reflectList(repeatedStringField, []);
+      expect(a.size).toBe(0);
+      const b = reflectList(repeatedStringField, ["a", "b"]);
+      expect(b.size).toBe(2);
+    });
+  });
+  describe("get()", () => {
+    test("returns item at index", () => {
+      const list = reflectList(repeatedStringField, ["a", "b"]);
+      expect(list.get(1)).toBe("b");
+    });
+    test("returns undefined if out of range", () => {
+      const list = reflectList(repeatedStringField, []);
+      expect(list.get(0)).toBeUndefined();
+    });
+    test("converts jstype=JS_STRING to bigint", () => {
+      const local: unknown[] = ["1"];
+      const list = reflectList(repeatedInt64JsStringField, local);
+      expect(list.get(0)).toBe(n1);
+    });
+  });
+  describe("add()", () => {
+    test("adds item", () => {
+      const local: unknown[] = ["a"];
+      const list = reflectList(repeatedStringField, local);
+      expect(list.add("b")).toBeUndefined();
+      expect(list.add("c")).toBeUndefined();
+      expect(local).toStrictEqual(["a", "b", "c"]);
+    });
+    test("adds items", () => {
+      const local: unknown[] = [];
+      const list = reflectList(repeatedStringField, local);
+      expect(list.add("a", "b", "c")).toBeUndefined();
+      expect(local).toStrictEqual(["a", "b", "c"]);
+    });
+    test("does not add any item if one of several items is invalid", () => {
+      const local: unknown[] = [];
+      const list = reflectList(repeatedStringField, local);
+      expect(list.add("a", "b", true)).toBeDefined();
+      expect(local).toStrictEqual([]);
+    });
+    test("converts number, string, bigint to bigint for 64-bit integer field", () => {
+      const local: unknown[] = [];
+      const list = reflectList(repeatedInt64Field, local);
+      expect(list.add(1)).toBeUndefined();
+      expect(list.add("2")).toBeUndefined();
+      expect(list.add(n3)).toBeUndefined();
+      expect(local).toStrictEqual([n1, n2, n3]);
+    });
+    test("converts number, string, bigint to string for 64-bit integer field with jstype=JS_STRING", () => {
+      const local: unknown[] = [];
+      const list = reflectList(repeatedInt64JsStringField, local);
+      expect(list.add(1)).toBeUndefined();
+      expect(list.add("2")).toBeUndefined();
+      expect(list.add(n3)).toBeUndefined();
+      expect(local).toStrictEqual(["1", "2", "3"]);
+    });
+    test("returns error for wrong message type", () => {
+      const list = reflectList(repeatedMessageField, []);
+      const err = list.add(reflectMessage(UserDesc));
+      expect(err?.message).toMatch(
+        /^list item #1: expected ReflectMessage \(spec.Proto3Message\), got ReflectMessage \(docs.User\)$/,
+      );
+    });
+    test("returns error for invalid scalar", () => {
+      const list = reflectList(repeatedStringField, []);
+      const err = list.add(true);
+      expect(err?.message).toMatch(/^list item #1: expected string, got true$/);
+    });
+  });
+  describe("set()", () => {
+    test("replaces item at index", () => {
+      const local: unknown[] = ["a", "b"];
+      const list = reflectList(repeatedStringField, local);
+      expect(list.set(0, "c")).toBeUndefined();
+      expect(local).toStrictEqual(["c", "b"]);
+    });
+    test("converts number, string, bigint to bigint for 64-bit integer field", () => {
+      const local: unknown[] = [n0, n0, n0];
+      const list = reflectList(repeatedInt64Field, local);
+      expect(list.set(0, 1)).toBeUndefined();
+      expect(list.set(1, "2")).toBeUndefined();
+      expect(list.set(2, n3)).toBeUndefined();
+      expect(local).toStrictEqual([n1, n2, n3]);
+    });
+    test("converts number, string, bigint to string for 64-bit integer field with jstype=JS_STRING", () => {
+      const local: unknown[] = ["0", "0", "0"];
+      const list = reflectList(repeatedInt64JsStringField, local);
+      expect(list.set(0, 1)).toBeUndefined();
+      expect(list.set(1, "2")).toBeUndefined();
+      expect(list.set(2, n3)).toBeUndefined();
+      expect(local).toStrictEqual(["1", "2", "3"]);
+    });
+    test("returns error if out of range", () => {
+      const list = reflectList(repeatedStringField, []);
+      const err = list.set(0, "abc");
+      expect(err?.message).toMatch(/^list item #1: out of range$/);
+    });
+    test("returns error for invalid scalar", () => {
+      const list = reflectList(repeatedStringField, [null]);
+      const err = list.set(0, true);
+      expect(err?.message).toMatch(/^list item #1: expected string, got true$/);
+    });
+    test("returns error for wrong message type", () => {
+      const list = reflectList(repeatedMessageField, [null]);
+      const err = list.set(0, reflectMessage(UserDesc));
+      expect(err?.message).toMatch(
+        /^list item #1: expected ReflectMessage \(spec.Proto3Message\), got ReflectMessage \(docs.User\)$/,
+      );
+    });
+  });
+  describe("clear()", () => {
+    test("removes all items", () => {
+      const local: unknown[] = ["a", "b"];
+      const list = reflectList(repeatedStringField, local);
+      list.clear();
+      expect(local).toStrictEqual([]);
+    });
+  });
+  describe("values()", () => {
+    test("returns iterable items", () => {
+      const local: unknown[] = ["a", "b"];
+      const values = reflectList(repeatedStringField, local).values();
+      expect(Array.from(values)).toStrictEqual(["a", "b"]);
+    });
+  });
+  describe("iterator", () => {
+    test("returns iterable items", () => {
+      const local: unknown[] = ["a", "b"];
+      const list = reflectList(repeatedStringField, local);
+      expect(Array.from(list)).toStrictEqual(["a", "b"]);
+    });
+  });
+  describe("entries()", () => {
+    test("returns iterable tuples", () => {
+      const local: unknown[] = ["a", "b"];
+      const entries = reflectList(repeatedStringField, local).entries();
+      expect(Array.from(entries)).toStrictEqual([
+        [0, "a"],
+        [1, "b"],
+      ]);
+    });
+  });
+  describe("keys()", () => {
+    test("returns iterable indices", () => {
+      const local: unknown[] = ["a", "b"];
+      const keys = reflectList(repeatedStringField, local).keys();
+      expect(Array.from(keys)).toStrictEqual([0, 1]);
+    });
+  });
+});

commit 48265554913ccaff085abbd61a8f4d5ea9c26746

Unskip skipped tests
Most importantly, tests for serializing map fields.

Fixes a bug in fromBinary, where map keys were converted to strings, which are the correct representation in a message, but not for a ReflectMap. This bug was most likely introduced in 7054ed75, which limited the conversion to a smaller set of acceptable types.

packages/protobuf/src/next/from-binary.ts
@@ -15,7 +15,7 @@
 import type { BinaryReadOptions } from "../binary-format.js";
 import type { DescField, DescMessage } from "../descriptor-set.js";
 import type { MessageShape } from "./types.js";
-import type { ReflectMessage } from "./reflect/index.js";
+import type { MapEntryKey, ReflectMessage } from "./reflect/index.js";
 import type { IBinaryReader } from "../binary-encoding.js";
 import {
   LongType,
@@ -140,7 +140,7 @@ function readField(
       message.set(field, readScalar(reader, field.scalar, field.longType));
       break;
     case "enum":
-      message.set(field, readScalar(reader, ScalarType.INT32) as number);
+      message.set(field, readScalar(reader, ScalarType.INT32));
       break;
     case "message":
       message.set(
@@ -166,10 +166,10 @@ function readMapEntry(
   field: DescField & { fieldKind: "map" },
   reader: IBinaryReader,
   options: BinaryReadOptions,
-): [string | number, ScalarValue | ReflectMessage] {
+): [MapEntryKey, ScalarValue | ReflectMessage] {
   const length = reader.uint32(),
     end = reader.pos + length;
-  let key: ScalarValue | undefined,
+  let key: MapEntryKey | undefined,
     val: ScalarValue | ReflectMessage | undefined;
   while (reader.pos < end) {
     const [fieldNo] = reader.tag();
@@ -194,9 +194,6 @@ function readMapEntry(
   }
   if (key === undefined) {
     key = scalarZeroValue(field.mapKey, LongType.BIGINT);
-  }
-  if (typeof key != "string" && typeof key != "number") {
-    key = key.toString();
   }
   if (val === undefined) {
     switch (field.mapKind) {
@@ -267,16 +264,16 @@ function readMessageField<Desc extends DescMessage>(
   return message;
 }
 
-function readScalar(
+function readScalar<Scalar extends ScalarType, L extends LongType>(
   reader: IBinaryReader,
-  type: ScalarType,
-  longType?: LongType,
-): ScalarValue {
+  type: Scalar,
+  longType?: L,
+): ScalarValue<Scalar, L> {
   let v = readScalarValue(reader, type);
   if (longType === LongType.STRING) {
     v = typeof v == "bigint" ? v.toString() : v;
   }
-  return v;
+  return v as ScalarValue<Scalar, L>;
 }
 
 function readScalarValue(reader: IBinaryReader, type: ScalarType): ScalarValue {

packages/protobuf-test/src/next/binary.test.ts
@@ -68,8 +68,7 @@ describe("binary serialization", () => {
         },
       );
     });
-    // TODO: `create` doesn't support maps yet, remove after support is added.
-    test.skip("for map fields", () => {
+    test("for map fields", () => {
       testV1Compat(MapsMessage, MapsMessageDesc, {
         boolStrField: { true: "foo" },
         int32MsgField: { 123: { strStrField: { key: "value" } } },

packages/protobuf-test/src/next/create.test.ts
@@ -409,152 +409,6 @@ describe("create()", () => {
       });
     });
     describe("64-bit integer field", () => {
-      function expect64BitFields(msg: proto3_ts.Proto3Message) {
-        expect(msg.singularInt64Field).toBe(protoInt64.parse(1));
-        expect(msg.singularInt64JsNumberField).toBe(protoInt64.parse(2));
-        expect(msg.singularInt64JsStringField).toBe("3");
-        expect(msg.repeatedInt64Field).toStrictEqual([protoInt64.parse(4)]);
-        expect(msg.repeatedInt64JsNumberField).toStrictEqual([
-          protoInt64.parse(5),
-        ]);
-        expect(msg.repeatedInt64JsStringField).toStrictEqual(["6"]);
-        expect(msg.either).toStrictEqual({
-          case: "oneofInt64Field",
-          value: protoInt64.parse(7),
-        });
-        expect(msg.singularMessageField?.either).toStrictEqual({
-          case: "oneofInt64JsNumberField",
-          value: protoInt64.parse(8),
-        });
-        expect(
-          msg.singularMessageField?.singularMessageField?.either,
-        ).toStrictEqual({ case: "oneofInt64JsStringField", value: "9" });
-        expect(msg.mapInt64Int64Field).toStrictEqual({
-          "1": protoInt64.parse(11),
-          "2": protoInt64.parse(22),
-        });
-      }
-      test.skip("accept bigint", () => {
-        const msg = create(proto3_ts.Proto3MessageDesc, {
-          singularInt64Field: protoInt64.parse(1),
-          singularInt64JsNumberField: protoInt64.parse(2),
-          // @ts-expect-error TS2322
-          singularInt64JsStringField: protoInt64.parse(3),
-          repeatedInt64Field: [protoInt64.parse(4)],
-          repeatedInt64JsNumberField: [protoInt64.parse(5)],
-          // @ts-expect-error TS2322
-          repeatedInt64JsStringField: [protoInt64.parse(6)],
-          either: {
-            case: "oneofInt64Field",
-            value: protoInt64.parse(7),
-          },
-          singularMessageField: {
-            either: {
-              case: "oneofInt64JsNumberField",
-              value: protoInt64.parse(8),
-            },
-            singularMessageField: {
-              either: {
-                // @ts-ignore -- required for older TS
-                case: "oneofInt64JsStringField",
-                // @ts-expect-error TS2322
-                value: protoInt64.parse(9),
-              },
-            },
-          },
-          mapInt64Int64Field: {
-            "1": protoInt64.parse(11),
-            "2": protoInt64.parse(22),
-          },
-        });
-        expect64BitFields(msg);
-      });
-      test.skip("accept number", () => {
-        const msg = create(proto3_ts.Proto3MessageDesc, {
-          // @ts-expect-error TS2322
-          singularInt64Field: 1,
-          // @ts-expect-error TS2322
-          singularInt64JsNumberField: 2,
-          // @ts-expect-error TS2322
-          singularInt64JsStringField: 3,
-          // @ts-expect-error TS2322
-          repeatedInt64Field: [4],
-          // @ts-expect-error TS2322
-          repeatedInt64JsNumberField: [5],
-          // @ts-expect-error TS2322
-          repeatedInt64JsStringField: [6],
-          either: {
-            // @ts-ignore -- required for older TS
-            case: "oneofInt64Field",
-            // @ts-expect-error TS2322
-            value: 7,
-          },
-          singularMessageField: {
-            either: {
-              // @ts-ignore -- required for older TS
-              case: "oneofInt64JsNumberField",
-              // @ts-expect-error TS2322
-              value: 8,
-            },
-            singularMessageField: {
-              either: {
-                // @ts-ignore -- required for older TS
-                case: "oneofInt64JsStringField",
-                // @ts-expect-error TS2322
-                value: 9,
-              },
-            },
-          },
-          mapInt64Int64Field: {
-            // @ts-ignore -- required for older TS
-            "1": 11,
-            // @ts-ignore -- required for older TS
-            "2": 22,
-          },
-        });
-        expect64BitFields(msg);
-      });
-      test.skip("accept string", () => {
-        const msg = create(proto3_ts.Proto3MessageDesc, {
-          // @ts-expect-error TS2322
-          singularInt64Field: "1",
-          // @ts-expect-error TS2322
-          singularInt64JsNumberField: "2",
-          singularInt64JsStringField: "3",
-          // @ts-expect-error TS2322
-          repeatedInt64Field: ["4"],
-          // @ts-expect-error TS2322
-          repeatedInt64JsNumberField: ["5"],
-          repeatedInt64JsStringField: ["6"],
-          either: {
-            // @ts-ignore -- required for older TS
-            case: "oneofInt64Field",
-            // @ts-expect-error TS2322
-            value: "7",
-          },
-          singularMessageField: {
-            either: {
-              // @ts-ignore -- required for older TS
-              case: "oneofInt64JsNumberField",
-              // @ts-expect-error TS2322
-              value: "8",
-            },
-            singularMessageField: {
-              either: {
-                case: "oneofInt64JsStringField",
-                value: "9",
-              },
-            },
-          },
-          mapInt64Int64Field: {
-            // @ts-ignore -- required for older TS
-            "1": "11",
-            // @ts-ignore -- required for older TS
-            "2": "22",
-          },
-        });
-        expect64BitFields(msg);
-      });
       test("accepts generated types", () => {
         const msg = create(proto3_ts.Proto3MessageDesc, {
           singularInt64Field: protoInt64.parse(1),
@@ -589,7 +443,7 @@ describe("create()", () => {
           "2": protoInt64.parse(9),
         });
       });
-      test("rejects other forms as type error", () => {
+      test("rejects other forms as type error but does not raise runtime error", () => {
         const msg = create(proto3_ts.Proto3MessageDesc, {
           // @ts-expect-error expected type error
           singularInt64Field: "1",
@@ -617,6 +471,22 @@ describe("create()", () => {
           },
         });
         expect(msg).toBeDefined();
+        expect(msg.singularInt64Field).toBe("1");
+        expect(msg.singularInt64JsNumberField).toBe("2");
+        expect(msg.singularInt64JsStringField).toBe(protoInt64.parse(3));
+        expect(msg.repeatedInt64Field).toStrictEqual([4]);
+        expect(msg.repeatedInt64JsNumberField).toStrictEqual([5]);
+        expect(msg.repeatedInt64JsStringField).toStrictEqual([
+          protoInt64.parse(6),
+        ]);
+        expect(msg.either).toStrictEqual({
+          case: "oneofInt64Field",
+          value: 7,
+        });
+        expect(msg.mapInt64Int64Field).toStrictEqual({
+          "1": 8,
+          "2": 9,
+        });
       });
     });
     describe("bytes", () => {

packages/protobuf-test/src/next/json.test.ts
@@ -82,8 +82,7 @@ describe("json serialization", () => {
         },
       );
     });
-    // TODO: `create` doesn't support maps yet, remove skip after support is added.
-    test.skip("for map fields", () => {
+    test("for map fields", () => {
       testV1Compat(MapsMessage, MapsMessageDesc, {
         boolStrField: { true: "foo" },
         int32MsgField: { 123: { strStrField: { key: "value" } } },
@@ -102,8 +101,7 @@ describe("json serialization", () => {
         const any = create(AnyDesc);
         expect(toJson(AnyDesc, any)).toStrictEqual({});
       });
-      // TODO: `create` doesn't support maps yet, remove skip after support is added.
-      test.skip(`encodes ${ValueDesc.typeName} with ${StructDesc.typeName} to JSON`, () => {
+      test(`encodes ${ValueDesc.typeName} with ${StructDesc.typeName} to JSON`, () => {
         const any = anyPack(
           ValueDesc,
           create(ValueDesc, {