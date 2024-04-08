34accf94e8467156ced26e213b1fd3a8d4869093
Timo Stamm <ts@timostamm.de>
2024-04-08 23:06:48 +0200

    Run v2 plugin with v2 function

--- packages/protoplugin/src/next/plugin.ts
+++ packages/protoplugin/src/next/plugin.ts
@@ -12,4 +12,27 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export * from "../plugin.js";
+import type {
+  CodeGeneratorRequest,
+  CodeGeneratorResponse,
+} from "@bufbuild/protobuf";
+
+/**
+ * Represents any code generator plugin.
+ */
+export interface Plugin {
+  /**
+   * Name of this code generator plugin.
+   */
+  name: string;
+
+  /**
+   * Version of this code generator plugin.
+   */
+  version: string;
+
+  /**
+   * Run this plugin for the given request.
+   */
+  run(request: CodeGeneratorRequest): CodeGeneratorResponse;
+}
--- packages/protoplugin/src/next/run-node.ts
+++ packages/protoplugin/src/next/run-node.ts
@@ -12,4 +12,79 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export * from "../run-node.js";
+import type { Plugin } from "./plugin.js";
+import type { ReadStream, WriteStream } from "tty";
+import { CodeGeneratorRequest } from "@bufbuild/protobuf";
+import { isPluginOptionError, reasonToString } from "./error.js";
+
+/**
+ * Run a plugin with Node.js.
+ *
+ * ```
+ * #!/usr/bin/env node
+ * const {runNodeJs} = require("@bufbuild/protoplugin");
+ * const {myPlugin} = require("./protoc-gen-x-plugin.js");
+ * runNodeJs(myPlugin);
+ * ```
+ */
+export function runNodeJs(plugin: Plugin): void {
+  const args = process.argv.slice(2);
+  if ((args.length === 1 && args[0] === "-v") || args[0] === "--version") {
+    process.stdout.write(`${plugin.name} ${plugin.version}\n`);
+    process.exit(0);
+    return;
+  }
+  if (args.length !== 0) {
+    process.stderr.write(
+      `${plugin.name} accepts a google.protobuf.compiler.CodeGeneratorRequest on stdin and writes a CodeGeneratorResponse to stdout\n`,
+    );
+    process.exit(1);
+    return;
+  }
+  readBytes(process.stdin)
+    .then((data) => {
+      const req = CodeGeneratorRequest.fromBinary(data);
+      const res = plugin.run(req);
+      return writeBytes(process.stdout, res.toBinary());
+    })
+    .then(() => process.exit(0))
+    .catch((reason) => {
+      const message = isPluginOptionError(reason)
+        ? reason.message
+        : reasonToString(reason);
+      process.stderr.write(`${plugin.name}: ${message}\n`);
+      process.exit(1);
+      return;
+    });
+}
+
+/**
+ * Read a stream to the end.
+ */
+function readBytes(stream: ReadStream): Promise<Uint8Array> {
+  return new Promise<Uint8Array>((resolve, reject) => {
+    const chunks: Uint8Array[] = [];
+    stream.on("data", (chunk) => chunks.push(chunk));
+    stream.on("end", () => {
+      resolve(Buffer.concat(chunks));
+    });
+    stream.on("error", (err) => {
+      reject(err);
+    });
+  });
+}
+
+/**
+ * Write a chunk of bytes to a stream.
+ */
+function writeBytes(stream: WriteStream, data: Uint8Array): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    stream.write(data, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
