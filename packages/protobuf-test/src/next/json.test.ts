7602116167ab79a13da074c3cd8f37c0dc8404a3
Sri Krishna Paritala <skrishna@buf.build>
2024-04-15 22:07:54 +0530

    Use test wrapper

--- packages/protobuf-test/src/next/json.test.ts
+++ packages/protobuf-test/src/next/json.test.ts
@@ -52,232 +52,216 @@ import { JSTypeProto2NormalMessageDesc } from "../gen/ts/extra/jstype-proto2_pbv
 import { fromJsonString } from "@bufbuild/protobuf/next";
 
 describe(`json serialization`, () => {
-  test("scalar fields", () => {
-    assertJson(
-      ScalarValuesMessageDesc,
-      {
-        doubleField: 0.75,
-        floatField: -0.75,
-        int64Field: protoInt64.parse(-1),
-        uint64Field: protoInt64.uParse(1),
-        int32Field: -123,
-        fixed64Field: protoInt64.uParse(1),
-        fixed32Field: 123,
-        boolField: true,
-        stringField: "hello world",
-        bytesField: new Uint8Array([
+  testJson(
+    ScalarValuesMessageDesc,
+    {
+      doubleField: 0.75,
+      floatField: -0.75,
+      int64Field: protoInt64.parse(-1),
+      uint64Field: protoInt64.uParse(1),
+      int32Field: -123,
+      fixed64Field: protoInt64.uParse(1),
+      fixed32Field: 123,
+      boolField: true,
+      stringField: "hello world",
+      bytesField: new Uint8Array([
+        104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100,
+      ]),
+      uint32Field: 123,
+      sfixed32Field: -123,
+      sfixed64Field: protoInt64.parse(-1),
+      sint32Field: -1,
+      sint64Field: protoInt64.parse(-1),
+    },
+    {
+      doubleField: 0.75,
+      floatField: -0.75,
+      int64Field: "-1",
+      uint64Field: "1",
+      int32Field: -123,
+      fixed64Field: "1",
+      fixed32Field: 123,
+      boolField: true,
+      stringField: "hello world",
+      bytesField: "aGVsbG8gd29ybGQ=",
+      uint32Field: 123,
+      sfixed32Field: -123,
+      sfixed64Field: "-1",
+      sint32Field: -1,
+      sint64Field: "-1",
+    },
+  );
+  testJson(
+    RepeatedScalarValuesMessageDesc,
+    {
+      doubleField: [0.75, 0, 1],
+      floatField: [0.75, -0.75],
+      int64Field: [protoInt64.parse(-1), protoInt64.parse(-2)],
+      uint64Field: [protoInt64.uParse(1), protoInt64.uParse(2)],
+      int32Field: [-123, 500],
+      fixed64Field: [protoInt64.uParse(1), protoInt64.uParse(99)],
+      fixed32Field: [123, 999],
+      boolField: [true, false, true],
+      stringField: ["hello", "world"],
+      bytesField: [
+        new Uint8Array([104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100]),
+      ],
+      uint32Field: [123, 123],
+      sfixed32Field: [-123, -123, -123],
+      sfixed64Field: [
+        protoInt64.parse(-1),
+        protoInt64.parse(-2),
+        protoInt64.parse(100),
+      ],
+      sint32Field: [-1, -2, 999],
+      sint64Field: [
+        protoInt64.parse(-1),
+        protoInt64.parse(-99),
+        protoInt64.parse(99),
+      ],
+    },
+    {
+      doubleField: [0.75, 0, 1],
+      floatField: [0.75, -0.75],
+      int64Field: ["-1", "-2"],
+      uint64Field: ["1", "2"],
+      int32Field: [-123, 500],
+      fixed64Field: ["1", "99"],
+      fixed32Field: [123, 999],
+      boolField: [true, false, true],
+      stringField: ["hello", "world"],
+      bytesField: ["aGVsbG8gd29ybGQ="],
+      uint32Field: [123, 123],
+      sfixed32Field: [-123, -123, -123],
+      sfixed64Field: ["-1", "-2", "100"],
+      sint32Field: [-1, -2, 999],
+      sint64Field: ["-1", "-99", "99"],
+    },
+  );
+  testJson(
+    MessageFieldMessageDesc,
+    {
+      messageField: { name: "test" },
+      repeatedMessageField: [{ name: "a" }, { name: "b" }],
+    },
+    {
+      messageField: { name: "test" },
+      repeatedMessageField: [{ name: "a" }, { name: "b" }],
+    },
+  );
+  testJson(
+    MapsMessageDesc,
+    {
+      strStrField: { a: "str", b: "xx" },
+      strInt32Field: { a: 123, b: 455 },
+      strInt64Field: { a: protoInt64.parse(123) },
+      strBoolField: { a: true, b: false },
+      strBytesField: {
+        a: new Uint8Array([
           104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100,
         ]),
-        uint32Field: 123,
-        sfixed32Field: -123,
-        sfixed64Field: protoInt64.parse(-1),
-        sint32Field: -1,
-        sint64Field: protoInt64.parse(-1),
       },
-      {
-        doubleField: 0.75,
-        floatField: -0.75,
-        int64Field: "-1",
-        uint64Field: "1",
-        int32Field: -123,
-        fixed64Field: "1",
-        fixed32Field: 123,
-        boolField: true,
-        stringField: "hello world",
-        bytesField: "aGVsbG8gd29ybGQ=",
-        uint32Field: 123,
-        sfixed32Field: -123,
-        sfixed64Field: "-1",
-        sint32Field: -1,
-        sint64Field: "-1",
+      int32StrField: { 123: "hello" },
+      int64StrField: { "9223372036854775807": "hello" },
+      boolStrField: { true: "yes", false: "no" },
+      strMsgField: {
+        a: {},
       },
-    );
-  });
-  test("repeated scalar fields", () => {
-    assertJson(
-      RepeatedScalarValuesMessageDesc,
-      {
-        doubleField: [0.75, 0, 1],
-        floatField: [0.75, -0.75],
-        int64Field: [protoInt64.parse(-1), protoInt64.parse(-2)],
-        uint64Field: [protoInt64.uParse(1), protoInt64.uParse(2)],
-        int32Field: [-123, 500],
-        fixed64Field: [protoInt64.uParse(1), protoInt64.uParse(99)],
-        fixed32Field: [123, 999],
-        boolField: [true, false, true],
-        stringField: ["hello", "world"],
-        bytesField: [
-          new Uint8Array([
-            104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100,
-          ]),
-        ],
-        uint32Field: [123, 123],
-        sfixed32Field: [-123, -123, -123],
-        sfixed64Field: [
-          protoInt64.parse(-1),
-          protoInt64.parse(-2),
-          protoInt64.parse(100),
-        ],
-        sint32Field: [-1, -2, 999],
-        sint64Field: [
-          protoInt64.parse(-1),
-          protoInt64.parse(-99),
-          protoInt64.parse(99),
-        ],
+      int32MsgField: {
+        "32": {},
       },
-      {
-        doubleField: [0.75, 0, 1],
-        floatField: [0.75, -0.75],
-        int64Field: ["-1", "-2"],
-        uint64Field: ["1", "2"],
-        int32Field: [-123, 500],
-        fixed64Field: ["1", "99"],
-        fixed32Field: [123, 999],
-        boolField: [true, false, true],
-        stringField: ["hello", "world"],
-        bytesField: ["aGVsbG8gd29ybGQ="],
-        uint32Field: [123, 123],
-        sfixed32Field: [-123, -123, -123],
-        sfixed64Field: ["-1", "-2", "100"],
-        sint32Field: [-1, -2, 999],
-        sint64Field: ["-1", "-99", "99"],
+      int64MsgField: {
+        "64": {},
       },
-    );
-  });
-  test("message fields", () => {
-    assertJson(
-      MessageFieldMessageDesc,
-      {
-        messageField: { name: "test" },
-        repeatedMessageField: [{ name: "a" }, { name: "b" }],
+      strEnuField: { a: 0, b: 1, c: 2 },
+      int32EnuField: { 1: 0, 2: 1, 0: 2 },
+      int64EnuField: { "-1": 0, "2": 1, "0": 2 },
+    },
+    {
+      strStrField: { a: "str", b: "xx" },
+      strInt32Field: { a: 123, b: 455 },
+      strInt64Field: { a: "123" },
+      strBoolField: { a: true, b: false },
+      strBytesField: { a: "aGVsbG8gd29ybGQ=" },
+      int32StrField: { "123": "hello" },
+      int64StrField: { "9223372036854775807": "hello" },
+      boolStrField: { true: "yes", false: "no" },
+      strMsgField: { a: {} },
+      int32MsgField: { "32": {} },
+      int64MsgField: { "64": {} },
+      strEnuField: {
+        a: "MAPS_ENUM_ANY",
+        b: "MAPS_ENUM_YES",
+        c: "MAPS_ENUM_NO",
       },
-      {
-        messageField: { name: "test" },
-        repeatedMessageField: [{ name: "a" }, { name: "b" }],
+      int32EnuField: {
+        "0": "MAPS_ENUM_NO",
+        "1": "MAPS_ENUM_ANY",
+        "2": "MAPS_ENUM_YES",
       },
-    );
-  });
-  test("map fields", () => {
-    assertJson(
-      MapsMessageDesc,
-      {
-        strStrField: { a: "str", b: "xx" },
-        strInt32Field: { a: 123, b: 455 },
-        strInt64Field: { a: protoInt64.parse(123) },
-        strBoolField: { a: true, b: false },
-        strBytesField: {
-          a: new Uint8Array([
-            104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100,
-          ]),
-        },
-        int32StrField: { 123: "hello" },
-        int64StrField: { "9223372036854775807": "hello" },
-        boolStrField: { true: "yes", false: "no" },
-        strMsgField: {
-          a: {},
-        },
-        int32MsgField: {
-          "32": {},
-        },
-        int64MsgField: {
-          "64": {},
-        },
-        strEnuField: { a: 0, b: 1, c: 2 },
-        int32EnuField: { 1: 0, 2: 1, 0: 2 },
-        int64EnuField: { "-1": 0, "2": 1, "0": 2 },
+      int64EnuField: {
+        "0": "MAPS_ENUM_NO",
+        "2": "MAPS_ENUM_YES",
+        "-1": "MAPS_ENUM_ANY",
       },
-      {
-        strStrField: { a: "str", b: "xx" },
-        strInt32Field: { a: 123, b: 455 },
-        strInt64Field: { a: "123" },
-        strBoolField: { a: true, b: false },
-        strBytesField: { a: "aGVsbG8gd29ybGQ=" },
-        int32StrField: { "123": "hello" },
-        int64StrField: { "9223372036854775807": "hello" },
-        boolStrField: { true: "yes", false: "no" },
-        strMsgField: { a: {} },
-        int32MsgField: { "32": {} },
-        int64MsgField: { "64": {} },
-        strEnuField: {
-          a: "MAPS_ENUM_ANY",
-          b: "MAPS_ENUM_YES",
-          c: "MAPS_ENUM_NO",
-        },
-        int32EnuField: {
-          "0": "MAPS_ENUM_NO",
-          "1": "MAPS_ENUM_ANY",
-          "2": "MAPS_ENUM_YES",
-        },
-        int64EnuField: {
-          "0": "MAPS_ENUM_NO",
-          "2": "MAPS_ENUM_YES",
-          "-1": "MAPS_ENUM_ANY",
+    },
+  );
+  testJson(
+    OneofMessageDesc,
+    {
+      message: {
+        case: "foo",
+        value: {
+          name: "max",
+          toggle: false,
         },
       },
-    );
-  });
-  test("oneof fields", () => {
-    assertJson(
-      OneofMessageDesc,
-      {
-        message: {
-          case: "foo",
-          value: {
-            name: "max",
-            toggle: false,
-          },
-        },
-        scalar: { case: undefined },
-        enum: { case: undefined },
-      },
-      {
-        foo: { name: "max" },
-      },
-    );
-  });
-  test("supports json_name option", () => {
-    assertJson(
-      JsonNamesMessageDesc,
-      {
-        a: "a",
-        b: "b",
-        c: "c",
-      },
-      { "@type": "c", "": "b", a: "a" },
-    );
-  });
-  test("supports jstype option", () => {
-    assertJson(
-      JSTypeProto2NormalMessageDesc,
-      {
-        fixed64Field: protoInt64.uParse(123),
-        int64Field: protoInt64.parse(123),
-        sfixed64Field: protoInt64.parse(123),
-        sint64Field: protoInt64.parse(123),
-        uint64Field: protoInt64.uParse(123),
-        repeatedFixed64Field: [protoInt64.uParse(123)],
-        repeatedInt64Field: [protoInt64.parse(123)],
-        repeatedSfixed64Field: [protoInt64.parse(123)],
-        repeatedSint64Field: [protoInt64.parse(123)],
-        repeatedUint64Field: [protoInt64.uParse(123)],
-      },
-      {
-        fixed64Field: "123",
-        int64Field: "123",
-        sfixed64Field: "123",
-        sint64Field: "123",
-        uint64Field: "123",
-        repeatedFixed64Field: ["123"],
-        repeatedInt64Field: ["123"],
-        repeatedSfixed64Field: ["123"],
-        repeatedSint64Field: ["123"],
-        repeatedUint64Field: ["123"],
-      },
-    );
-  });
+      scalar: { case: undefined },
+      enum: { case: undefined },
+    },
+    {
+      foo: { name: "max" },
+    },
+  );
+  testJson(
+    JsonNamesMessageDesc,
+    {
+      a: "a",
+      b: "b",
+      c: "c",
+    },
+    { "@type": "c", "": "b", a: "a" },
+  );
+  testJson(
+    JSTypeProto2NormalMessageDesc,
+    {
+      fixed64Field: protoInt64.uParse(123),
+      int64Field: protoInt64.parse(123),
+      sfixed64Field: protoInt64.parse(123),
+      sint64Field: protoInt64.parse(123),
+      uint64Field: protoInt64.uParse(123),
+      repeatedFixed64Field: [protoInt64.uParse(123)],
+      repeatedInt64Field: [protoInt64.parse(123)],
+      repeatedSfixed64Field: [protoInt64.parse(123)],
+      repeatedSint64Field: [protoInt64.parse(123)],
+      repeatedUint64Field: [protoInt64.uParse(123)],
+    },
+    {
+      fixed64Field: "123",
+      int64Field: "123",
+      sfixed64Field: "123",
+      sint64Field: "123",
+      uint64Field: "123",
+      repeatedFixed64Field: ["123"],
+      repeatedInt64Field: ["123"],
+      repeatedSfixed64Field: ["123"],
+      repeatedSint64Field: ["123"],
+      repeatedUint64Field: ["123"],
+    },
+  );
   describe("wkt", () => {
     describe("wrappers", () => {
-      assertJson(
+      testJson(
         WrappersMessageDesc,
         {
           doubleValueField: 1.2,
@@ -406,14 +390,14 @@ describe(`json serialization`, () => {
       });
     });
     describe("Duration", () => {
-      const assertDurationJson = (
+      const testDurationJson = (
         init: MessageInitShape<typeof DurationDesc>,
         json: string,
       ) => {
-        assertJson(DurationDesc, init, json);
+        testJson(DurationDesc, init, json);
       };
-      test("3s", () => {
-        assertDurationJson(
+      describe("3s", () => {
+        testDurationJson(
           {
             seconds: protoInt64.parse(3),
             nanos: 0,
@@ -421,8 +405,8 @@ describe(`json serialization`, () => {
           "3s",
         );
       });
-      test("3s 1ms", () => {
-        assertDurationJson(
+      describe("3s 1ms", () => {
+        testDurationJson(
           {
             seconds: protoInt64.parse(3),
             nanos: 1000,
@@ -430,8 +414,8 @@ describe(`json serialization`, () => {
           "3.000001s",
         );
       });
-      test("3s 1ns", () => {
-        assertDurationJson(
+      describe("3s 1ns", () => {
+        testDurationJson(
           {
             seconds: protoInt64.parse(3),
             nanos: 1,
@@ -439,8 +423,8 @@ describe(`json serialization`, () => {
           "3.000000001s",
         );
       });
-      test("-3s 1ns", () => {
-        assertDurationJson(
+      describe("-3s 1ns", () => {
+        testDurationJson(
           {
             seconds: protoInt64.parse(-3),
             nanos: -1,
@@ -448,8 +432,8 @@ describe(`json serialization`, () => {
           "-3.000000001s",
         );
       });
-      test("0s 5ns", () => {
-        assertDurationJson(
+      describe("0s 5ns", () => {
+        testDurationJson(
           {
             seconds: protoInt64.parse(0),
             nanos: 5,
@@ -457,8 +441,8 @@ describe(`json serialization`, () => {
           "0.000000005s",
         );
       });
-      test("0s -5ns", () => {
-        assertDurationJson(
+      describe("0s -5ns", () => {
+        testDurationJson(
           {
             seconds: protoInt64.parse(0),
             nanos: -5,
@@ -467,19 +451,15 @@ describe(`json serialization`, () => {
         );
       });
     });
-    describe("Timestamp", () => {
-      assertJson(TimestampDesc, {}, "1970-01-01T00:00:00Z");
-    });
+    testJson(TimestampDesc, {}, "1970-01-01T00:00:00Z");
     describe("FieldMask", () => {
-      describe("valid", () => {
-        assertJson(
-          FieldMaskDesc,
-          {
-            paths: ["user.display_name", "photo"],
-          },
-          "user.displayName,photo",
-        );
-      });
+      testJson(
+        FieldMaskDesc,
+        {
+          paths: ["user.display_name", "photo"],
+        },
+        "user.displayName,photo",
+      );
       test("toJson fails on invalid fieldmask paths", () => {
         const fieldMask = create(FieldMaskDesc, {
           paths: ["user.displayName", "photo"],
@@ -499,28 +479,24 @@ describe(`json serialization`, () => {
         );
       });
     });
-    describe("Struct", () => {
-      assertJson(
-        StructDesc,
-        {
-          fields: {
-            a: { kind: { case: "numberValue", value: 123 } },
-            b: { kind: { case: "stringValue", value: "abc" } },
-          },
+    testJson(
+      StructDesc,
+      {
+        fields: {
+          a: { kind: { case: "numberValue", value: 123 } },
+          b: { kind: { case: "stringValue", value: "abc" } },
         },
-        { a: 123, b: "abc" },
-      );
-    });
+      },
+      { a: 123, b: "abc" },
+    );
     describe("Value", () => {
-      test("value", () => {
-        assertJson(
-          ValueDesc,
-          {
-            kind: { case: "boolValue", value: true },
-          },
-          true,
-        );
-      });
+      testJson(
+        ValueDesc,
+        {
+          kind: { case: "boolValue", value: true },
+        },
+        true,
+      );
       test("encoding unset value to JSON raises error", () => {
         // Absence of any variant indicates an error.
         // See struct.proto
@@ -567,7 +543,7 @@ describe(`json serialization`, () => {
         }).toThrowError("google.protobuf.Value cannot be NaN or Infinity");
       });
       describe("Value with Struct field", () => {
-        assertJson(
+        testJson(
           ValueDesc,
           {
             kind: {
@@ -603,12 +579,14 @@ describe(`json serialization`, () => {
   });
 });
 
-function assertJson<Desc extends DescMessage>(
+function testJson<Desc extends DescMessage>(
   desc: Desc,
   init: MessageInitShape<Desc>,
   json: JsonValue,
 ) {
-  const msg = create(desc, init);
-  expect(toJson(desc, msg)).toStrictEqual(json);
-  expect(fromJson(desc, json)).toStrictEqual(msg);
+  test(desc.typeName, () => {
+    const msg = create(desc, init);
+    expect(toJson(desc, msg)).toStrictEqual(json);
+    expect(fromJson(desc, json)).toStrictEqual(msg);
+  });
 }
