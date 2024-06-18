7d57629f266dc35bd848fc2701dcd8a1b56fb131
Timo Stamm <ts@timostamm.de>
2024-06-18 12:29:39 +0200

    Add example for a default registry with a custom toJson() function

---
+++ packages/protobuf-test/src/to-json-default-registry.test.ts
@@ -0,0 +1,55 @@
+// Copyright 2021-2024 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {
+  create,
+  createRegistry,
+  toJson as toJsonOriginal,
+} from "@bufbuild/protobuf";
+import { anyPack, AnySchema, DurationSchema } from "@bufbuild/protobuf/wkt";
+import { describe, expect, test } from "@jest/globals";
+
+/* eslint-disable @typescript-eslint/no-unsafe-return,@typescript-eslint/no-explicit-any */
+
+describe("default registry with custom toJson()", () => {
+  // Define you own standard registry
+  const registry = createRegistry(
+    // You can register as many types as you need. In this example, we only
+    // register google.protobuf.Duration.
+    DurationSchema,
+  );
+
+  // Define your own toJson function that uses your standard registry
+  const toJson: typeof toJsonOriginal = (schema, message, options) => {
+    return toJsonOriginal(
+      schema,
+      message,
+      options ?? { registry: registry },
+    ) as any;
+  };
+
+  test("works as expected", () => {
+    // Create a google.protobuf.Any that holds a google.protobuf.Duration
+    const duration = create(DurationSchema, { nanos: 123 });
+    const any = anyPack(DurationSchema, duration);
+
+    // Serializing to JSON requires a registry that contains google.protobuf.Duration
+    const json = toJson(AnySchema, any);
+
+    expect(json).toStrictEqual({
+      "@type": "type.googleapis.com/google.protobuf.Duration",
+      value: "0.000000123s",
+    });
+  });
+});
