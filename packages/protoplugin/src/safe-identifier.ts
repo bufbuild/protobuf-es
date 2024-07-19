1648dd484701a5542de0a5adc7d60485911c901f
Timo Stamm <ts@timostamm.de>
2024-07-19 13:43:35 +0200

    Keep also see
    Signed-off-by: Timo Stamm <ts@timostamm.de>

--- packages/protoplugin/src/safe-identifier.ts
+++ packages/protoplugin/src/safe-identifier.ts
@@ -88,6 +88,8 @@ const reservedIdentifiers = new Set([
  *
  * This function is intended for use with identifiers from Protobuf. The passed
  * string must be a valid identifier (e.g. not start with a digit).
+ *
+ * Also see safeObjectProperty() from @bufbuild/protoplugin/reflect.
  */
 export function safeIdentifier(name: string): string {
   return reservedIdentifiers.has(name) ? name + "$" : name;
