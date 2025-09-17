03e4b76c1b33a597f6217d66a2e3a38a4ba78124
Dan Hudlow <hudlow@buf.build>
2025-09-17 13:28:04 -0500

    remove out-of-place test

--- packages/typescript-composer/src/expr/id.test.ts
+++ packages/typescript-composer/src/expr/id.test.ts
@@ -1,6 +1,5 @@
 import assert from "node:assert";
 import { suite, test } from "node:test";
-import { access } from "./access.js";
 import { id, isId } from "./id.js";
 
 void suite("isId()", () => {
@@ -26,16 +25,6 @@ void suite("print", () => {
       id("myVar").toString(),
       "myVar",
       "A barebones `Id` instance must print correctly",
-    );
-  });
-  void test("dynamic expression property access", () => {
-    const base = id("foo");
-    const expected = access(base, "bar");
-
-    assert.deepEqual(
-      base.$bar,
-      expected,
-      '`x.$y` must be equivalent to `access(x, "y")`',
     );
   });
 });
