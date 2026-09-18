b52557368a81dc3622ac8f51b81dc7cb7c150325
Timo Stamm <ts@timostamm.de>
2026-09-18 13:54:35 +0200

    Update WKT import paths
    Pin codegenv1, and update codegenv2: Maintain imports from the barrel for all commonly used WKT. Use the full subpath for rarely used and new ones.

--- packages/protobuf/src/codegenv2/symbols.ts
+++ packages/protobuf/src/codegenv2/symbols.ts
@@ -17,25 +17,43 @@
  */
 export const packageName = "@bufbuild/protobuf";
 
+const legacyWktBarrel = packageName + "/wkt";
+
 /**
  * @private
+ *
+ * A map from Protobuf file path to import path for the well-known types.
+ *
+ * All well-known files are exported as subpaths from @bufbuild/protobuf/wkt,
+ * but have been historically exported from a barrel. For backwards-compatibility,
+ * the existing barrel exports are maintained.
+ *
+ * For convenience, generated code continues to import from the barrel for
+ * commonly used WKT, and only generates the full subpath import for files
+ * that are rarely used.
+ *
+ * All well-known files must be listed here. New files should import from the
+ * full subpath.
  */
 export const wktPublicImportPaths: Readonly<Record<string, string>> = {
-  "google/protobuf/compiler/plugin.proto": packageName + "/wkt",
-  "google/protobuf/any.proto": packageName + "/wkt",
-  "google/protobuf/api.proto": packageName + "/wkt",
-  "google/protobuf/cpp_features.proto": packageName + "/wkt",
-  "google/protobuf/descriptor.proto": packageName + "/wkt",
-  "google/protobuf/duration.proto": packageName + "/wkt",
-  "google/protobuf/empty.proto": packageName + "/wkt",
-  "google/protobuf/field_mask.proto": packageName + "/wkt",
-  "google/protobuf/go_features.proto": packageName + "/wkt",
-  "google/protobuf/java_features.proto": packageName + "/wkt",
-  "google/protobuf/source_context.proto": packageName + "/wkt",
-  "google/protobuf/struct.proto": packageName + "/wkt",
-  "google/protobuf/timestamp.proto": packageName + "/wkt",
-  "google/protobuf/type.proto": packageName + "/wkt",
-  "google/protobuf/wrappers.proto": packageName + "/wkt",
+  "google/protobuf/compiler/plugin.proto": legacyWktBarrel,
+  "google/protobuf/any.proto": legacyWktBarrel,
+  "google/protobuf/api.proto": packageName + "/wkt/google/protobuf/api_pb.js",
+  "google/protobuf/cpp_features.proto":
+    packageName + "/wkt/google/protobuf/cpp_features_pb.js",
+  "google/protobuf/descriptor.proto": legacyWktBarrel,
+  "google/protobuf/duration.proto": legacyWktBarrel,
+  "google/protobuf/empty.proto": legacyWktBarrel,
+  "google/protobuf/field_mask.proto": legacyWktBarrel,
+  "google/protobuf/go_features.proto":
+    packageName + "/wkt/google/protobuf/go_features_pb.js",
+  "google/protobuf/java_features.proto":
+    packageName + "/wkt/google/protobuf/java_features_pb.js",
+  "google/protobuf/source_context.proto": legacyWktBarrel,
+  "google/protobuf/struct.proto": legacyWktBarrel,
+  "google/protobuf/timestamp.proto": legacyWktBarrel,
+  "google/protobuf/type.proto": packageName + "/wkt/google/protobuf/type_pb.js",
+  "google/protobuf/wrappers.proto": legacyWktBarrel,
 };
 
 /**
